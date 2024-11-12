0a77177fb87974599312a0ef182f83a8a5dde983
tim goh <timslgoh@gmail.com>
2024-11-12 12:30:43 +0800

    01_building_abstractions_with_procedures/scratch.rkt

--- 01_building_abstractions_with_procedures/scratch.rkt
+++ 01_building_abstractions_with_procedures/scratch.rkt
@@ -1,5 +1,14 @@
 #lang sicp
 
-(define (square x) (* x x))
+;; (define (square x) (* x x))
+;; (square 21)
 
-(square 21)
+;; (define (abs x)
+;;   (cond 
+;;       ((> x 0) x)
+;;       ((= x 0) 0)
+;;       ((< x 0) (- x))
+;;   )
+;; )
+;;
+;; abs -3
