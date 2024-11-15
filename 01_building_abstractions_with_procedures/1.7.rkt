129a68f2b93fb3be24941a5fcd76e8ee0c0228c7
tim goh <timslgoh@gmail.com>
2024-11-15 11:55:03 +0800

    01_building_abstractions_with_procedures/1.7.rkt

--- 01_building_abstractions_with_procedures/1.7.rkt
+++ 01_building_abstractions_with_procedures/1.7.rkt
@@ -1,22 +1,23 @@
-
+#lang sicp
 
 ;; good enough
 (define (square x) (* x x))
 (define (abs x)
-  (cond 
-      ((> x 0) x)
-      ((= x 0) 0)
-      ((< x 0) (- x))
+  (cond
+    ((> x 0) x)
+    ((= x 0) 0)
+    ((< x 0) (- x))
+    )
   )
-)
 (define (good-enough? guess x)
-(< (abs (- (square guess) x)) 0.001))
+  (< (abs (- (square guess) x)) 0.001))
 
 ;; improve
 (define (average x y) (/ (+ x y) 2))
 (define (improve guess x) (average guess (/ x guess)))
 
-
-(define (sqrt-iter guess x) (if (good-enough? guess x)
-      guess
-      (sqrt-iter (improve guess x) x)))
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess (sqrt-iter (improve guess x) x)
+      )
+)
