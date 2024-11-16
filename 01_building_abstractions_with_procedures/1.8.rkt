72c9fd5607ce162431f433d138e1375dc184d580
tim goh <timslgoh@gmail.com>
2024-11-16 10:52:43 +0800

    01_building_abstractions_with_procedures/1.8.rkt

--- 01_building_abstractions_with_procedures/1.8.rkt
+++ 01_building_abstractions_with_procedures/1.8.rkt
@@ -2,6 +2,7 @@
 
 ;; good enough
 (define (square x) (* x x))
+(define (cube x) (* (* x x) x))
 (define (abs x)
   (cond
     ((> x 0) x)
@@ -10,14 +11,12 @@
     )
   )
 (define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001))
-
-(define (_good-enough? guess x)
-  (< (/ (abs (- x (square guess) )) x) 0.01))
+  (< (abs (- (cube guess) x)) 0.001))
 
 ;; improve
 (define (average x y) (/ (+ x y) 2))
-(define (improve guess x) (average guess (/ x guess)))
+;; (define (improve guess x) (average guess (/ x guess)))
+(define (improve guess x) (/ (+ (/ x (square guess)) (* 2 guess))  3 ))
 
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
@@ -25,3 +24,16 @@
       (sqrt-iter (improve guess x) x)
       )
   )
+
+(define (cubert-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cubert-iter (improve guess x) x)
+      )
+  )
+
+(cubert-iter 1.0 8.0)
+
+;; (improve 1 8)
+;; (improve 10/3 8)
+;; (improve 554/225 8)
