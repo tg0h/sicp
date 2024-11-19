b239fd385817c976e34b6c12406e27d2a01c52b2
tim goh <timslgoh@gmail.com>
2024-11-19 12:46:19 +0800

    01_building_abstractions_with_procedures/1.11.rkt

--- 01_building_abstractions_with_procedures/1.11.rkt
+++ 01_building_abstractions_with_procedures/1.11.rkt
@@ -20,11 +20,13 @@
 (define (sum a b c) (+ a (* 2 b) (* 3 c)))
 (define (f-iter counter n a b c)
   (cond
-    ((< n 3) n)
-    (= counter n) (+ sum a b c)
-    (else f-iter (+ counter 1) n (sum a b c) a b)
+    ((= counter n) (sum a b c))
+    (else (f-iter (+ counter 1) n (sum a b c) a b))
     )
   )
 
 
-(f-iter 3 5 2 1 0)
+(f-iter 3 4 2 1 0)
+;; (sum 2 1 0)
+;; (f-iter 4 4 4 2 1)
+;; (sum 2 1 0)
