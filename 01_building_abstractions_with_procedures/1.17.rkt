776fded2331f482b51d4f4c9e0590d610c4a4d69
tim goh <timslgoh@gmail.com>
2024-11-21 14:44:45 +0800

    01_building_abstractions_with_procedures/1.17.rkt

--- 01_building_abstractions_with_procedures/1.17.rkt
+++ 01_building_abstractions_with_procedures/1.17.rkt
@@ -1,4 +1,23 @@
 #lang sicp
 
-(define f-mult
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (f-mult a b )
+  (define (iter a b c k)
+    (display a)
+    (display ":" )
+    (display b)
+    (display ":" )
+    (display c)
+    (display ":" )
+    (display k)
+    (newline)
+    (cond ((= k 1) a)
+          ((not (even? k)) (f-exp (* a b) b (- k 1)))
+          (else (f-exp (* a (square b)) b (/ k 2)))
+          )
+    )
   )
+
+(f-exp 1 2 5 )
