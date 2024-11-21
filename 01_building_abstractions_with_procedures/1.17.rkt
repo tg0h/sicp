9f7a8bd5257b3865b564c773013ffba4065afb7c
tim goh <timslgoh@gmail.com>
2024-11-21 16:09:09 +0800

    01_building_abstractions_with_procedures/1.17.rkt

--- 01_building_abstractions_with_procedures/1.17.rkt
+++ 01_building_abstractions_with_procedures/1.17.rkt
@@ -12,20 +12,21 @@
   )
 
 (define (f-mult a b )
-  (define (iter a b c k)
+  (define (iter a b c product)
     (display a)
     (display ":" )
     (display b)
     (display ":" )
     (display c)
     (display ":" )
-    (display k)
+    (display product)
     (newline)
-    (cond ((= k 1) a)
-          ((not (even? k)) (f-exp (* a b) b (- k 1)))
-          (else (f-exp (* a (square b)) b (/ k 2)))
+    (cond ((= b 1) (+ c product))
+          ((even? b) (iter a (halve b) product (double a)) ); even
+          (else (iter a (- b 1) product product)) ; odd
           )
     )
+  (iter a b 0 1)
   )
 
-(f-mult 3 5)
+(f-mult 3 2)
