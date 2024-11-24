8f1e9b70b3d318de862cd9a83617b9388b308e13
tim goh <timslgoh@gmail.com>
2024-11-24 11:24:52 +0800

    01_building_abstractions_with_procedures/1.22.rkt

--- 01_building_abstractions_with_procedures/1.22.rkt
+++ 01_building_abstractions_with_procedures/1.22.rkt
@@ -2,17 +2,17 @@
 
 (define (square x) (* x x))
 (define (smallest-divisor n) (find-divisor n 2))
-
+(define (divides? a b) (= (remainder b a) 0))
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (+ test-divisor 1)))))
 
-(define (divides? a b) (= (remainder b a) 0))
 (define (prime? n)
   (= n (smallest-divisor n)))
 
-(define (timed-prime-test n) (newline)
+(define (timed-prime-test n)
+  (newline)
   (display n)
   (start-prime-test n (runtime)))
 
