32dece2cc735107b2e4847e88b81e57bbf64c5b5
tim goh <timslgoh@gmail.com>
2024-11-25 14:28:33 +0800

    1.25.rkt

---
+++ 1.25.rkt
@@ -0,0 +1,52 @@
+#lang sicp
+
+(define (square x) (* x x))
+(define (smallest-divisor n) (find-divisor n 2))
+(define (divides? a b) (= (remainder b a) 0))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  ;; (if (prime? n)
+  (if (fast-prime? n 100) ; use fermat test
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (search-for-primes lower upper)
+  (define (iter n)
+    (cond
+      ((> n upper) )
+      ((even? n) (iter (+ n 1)))
+      (else
+       (timed-prime-test n)
+       (iter (+ n 2)))
+      )
+    )
+  (iter lower)
+  )
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m)))
+  )
