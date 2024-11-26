ea150d1f8d92dd7950d73dbcaa515949d8e67489
tim goh <timslgoh@gmail.com>
2024-11-26 14:31:53 +0800

    01_building_abstractions_with_procedures/1.29.rkt

--- 01_building_abstractions_with_procedures/1.29.rkt
+++ 01_building_abstractions_with_procedures/1.29.rkt
@@ -16,3 +16,15 @@
 (define (cube x ) (* (* x x) x))
 
 (integral cube 0 1 0.01)
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (y-term f a k h n)
+  (cond
+    ((= k 1) (f (+ a (* k h))))
+    ((= k n) (f (+ a (* k h))))
+    ((even? k) (* 2 (f (+ a (* k h)))))
+    (else (* 4 (f (+ a (* k h)))))
+    )
+  )
