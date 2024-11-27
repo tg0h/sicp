0770929e3bd086046ce98ffc5bbd6044ad5a7d4e
tim goh <timslgoh@gmail.com>
2024-11-27 13:36:38 +0800

    01_building_abstractions_with_procedures/1.37.rkt

--- 01_building_abstractions_with_procedures/1.37.rkt
+++ 01_building_abstractions_with_procedures/1.37.rkt
@@ -1,12 +1,16 @@
 #lang sicp
 
-(define (cont-frac n d k )
-  (define (iter n d k i result)
-    (cond
-      ((> i k ) result)
-      (else (/ (n i) (d i)))
-      )
+;; (define (cont-frac n d k )
+;;   (define (iter n d k i result)
+;;     (cond
+;;       ((> i k ) result)
+;;       (else (/ (n i) (d i)) (iter ) )
+;;       )
+;;
+;;     )
+;;   (iter n d 3 1 1)
+;;   )
 
-    )
-  (iter n d 3 1 1)
+(define (cont-frac n d k)
+  (/ (n 1) (+ (d 1) (cont-frac n d (- k 1))))
   )
