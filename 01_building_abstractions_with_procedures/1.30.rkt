5751eb59e1f6c23f0530bab9a8200e42e8d18e68
tim goh <timslgoh@gmail.com>
2024-11-27 08:33:50 +0800

    01_building_abstractions_with_procedures/1.30.rkt

--- 01_building_abstractions_with_procedures/1.30.rkt
+++ 01_building_abstractions_with_procedures/1.30.rkt
@@ -23,13 +23,16 @@
 (define (next x)
   (+ x 1))
 
-(sum cube 1 next 2)
+(sum cube 1 next 9)
 
 (define (_sum term a next b)
   (define (iter a result)
     (if (> a b)
         result
-        (iter (next a) (+ a result)))
+        (iter (next a) (+ (term a) result)))
     )
   (iter a 0)
   )
+
+
+(_sum cube 1 next 9)
