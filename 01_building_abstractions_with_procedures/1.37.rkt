2c1ce9a8d17cba252f85984e28b0cde8b026c54b
tim goh <timslgoh@gmail.com>
2024-11-27 13:46:11 +0800

    01_building_abstractions_with_procedures/1.37.rkt

--- 01_building_abstractions_with_procedures/1.37.rkt
+++ 01_building_abstractions_with_procedures/1.37.rkt
@@ -12,5 +12,17 @@
 ;;   )
 
 (define (cont-frac n d k)
-  (/ (n 1) (+ (d 1) (cont-frac n d (- k 1))))
+  (if (= k 1) 1
+      (/ (n 1) (+ (d 1) (cont-frac n d (- k 1))))
+      )
   )
+
+;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 3)
+;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 4)
+;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 5)
+;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 10)
+;; (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 11)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 13)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 15)
+(cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 20)
