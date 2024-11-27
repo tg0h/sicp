37bb438d3b570dfa9405d858f92b3115ff514807
tim goh <timslgoh@gmail.com>
2024-11-27 10:23:22 +0800

    01_building_abstractions_with_procedures/1.32.rkt

--- 01_building_abstractions_with_procedures/1.32.rkt
+++ 01_building_abstractions_with_procedures/1.32.rkt
@@ -21,8 +21,8 @@
   (iter a 1)
   )
 
-(sum identity 1 next 4)
-(product identity 1 next 4)
+;; (sum identity 1 next 4)
+;; (product identity 1 next 4)
 
 (define (accumulate combiner null-value term a next b)
   (define (iter a result)
@@ -33,9 +33,25 @@
   (iter a null-value)
   )
 
-(define (plus x y ) (+ x y))
+(define (accumulate-recursive combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumulate-recursive combiner null-value term (next a ) next b))
+      )
+  )
 
 (accumulate + 0 identity 1 next 3)
+(accumulate-recursive + 0 identity 1 next 3)
+(newline)
+
 (accumulate + 0 identity 1 next 4)
+(accumulate-recursive + 0 identity 1 next 4)
+(newline)
+
 (accumulate * 1 identity 1 next 3)
+(accumulate-recursive * 1 identity 1 next 3)
+(newline)
+
 (accumulate * 1 identity 1 next 4)
+(accumulate-recursive * 1 identity 1 next 4)
+(newline)
