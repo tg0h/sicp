8d67fcd2c044224b10b8d28e5ecac8cc9e0b5d6b
tim goh <timslgoh@gmail.com>
2024-11-27 12:18:02 +0800

    01_building_abstractions_with_procedures/1.35.rkt

---
+++ 01_building_abstractions_with_procedures/1.35.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define tolerance 0.00001)
+
+(define (abs x)
+  (cond
+    ((> x 0) x)
+    ((= x 0) 0)
+    ((< x 0) (- x))
+    )
+  )
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
