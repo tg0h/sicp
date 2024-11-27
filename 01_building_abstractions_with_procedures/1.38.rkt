a1f994e56936cabdb3d1e008a0ca0290d0b1e5fc
tim goh <timslgoh@gmail.com>
2024-11-27 14:57:42 +0800

    01_building_abstractions_with_procedures/1.38.rkt

--- 01_building_abstractions_with_procedures/1.38.rkt
+++ 01_building_abstractions_with_procedures/1.38.rkt
@@ -5,3 +5,13 @@
       (/ (n 1) (+ (d 1) (cont-frac n d (- k 1))))
       )
   )
+
+(define (n i) 1.0)
+
+(define (d i)
+  (
+   cond ((= (remainder i 3) 1) 1)
+        ((= (remainder i 3) 0) 1)
+        (else (* 2 (/ (+ i 1) 3)))
+        )
+  )
