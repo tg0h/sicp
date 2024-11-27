41614248dee1fb66bd68ba783e3d092a9cdd904e
tim goh <timslgoh@gmail.com>
2024-11-27 09:26:14 +0800

    01_building_abstractions_with_procedures/1.31.rkt

--- 01_building_abstractions_with_procedures/1.31.rkt
+++ 01_building_abstractions_with_procedures/1.31.rkt
@@ -19,10 +19,23 @@
   (product even-y 1 next 3)
   )
 
-(even-mult 1 3)
+;; (even-mult 1 3)
 
 (define (odd-mult a b)
   (product odd-y 1 next 3)
   )
 
-(odd-mult 1 3)
+;; (odd-mult 1 3)
+
+(define (pi-approx n)
+  (*
+   ( /
+     (* (even-mult 1 n)
+        (/ (even-mult 1 (+ 1 n) 2)))
+     (* (odd-mult 1 n) (odd-mult 1 n))
+     )
+   4
+   )
+  )
+
+(pi-approx 3)
