0b109144ba495812197eb53d16f3be0f15e78f5c
tim goh <timslgoh@gmail.com>
2024-11-27 08:18:35 +0800

    01_building_abstractions_with_procedures/1.30.rkt

--- 01_building_abstractions_with_procedures/1.30.rkt
+++ 01_building_abstractions_with_procedures/1.30.rkt
@@ -18,34 +18,9 @@
   (* (sum f (+ a (/ dx 2.0)) add-dx b)
      dx))
 
-
 (define (cube x ) (* (* x x) x))
 
-(integral cube 0 1 0.01)
+(define (next x)
+  (+ x 1))
 
-(define (even? n)
-  (= (remainder n 2) 0))
-
-
-(define (simpsons-rule f a b n)
-  (define (h n) (/ (- b a) n))
-  (define (next current) (+ current 1))
-  (define (y-term k)
-    ;; (display "k:") (display k)
-    ;; (newline)
-    (cond
-      ((= k 0) (f a))
-      ((= k n) (f (+ a (* k (/ (- b a) n)))))
-      ((even? k) (* 2 (f (+ a (* k (/ (- b a) n))))))
-      (else (* 4 (f (+ a (* k (/ (- b a) n))))))
-      )
-    )
-  ;; (display (h n))
-  (/ (* (sum y-term a next n) (h n)) 3)
-  ;; (sum y-term a next n)
-  )
-
-;; (simpsons-rule cube 0 1 2)
-(simpsons-rule cube 0 1 100)
-(simpsons-rule cube 0 1 1000)
-;; (y-term cube 0 1 100 1)
+(sum cube 1 next 2)
