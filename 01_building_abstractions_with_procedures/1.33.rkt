8868f60079b90d2712c8f199736807fe03f6a561
tim goh <timslgoh@gmail.com>
2024-11-27 10:51:17 +0800

    01_building_abstractions_with_procedures/1.33.rkt

--- 01_building_abstractions_with_procedures/1.33.rkt
+++ 01_building_abstractions_with_procedures/1.33.rkt
@@ -43,14 +43,17 @@
   (iter a null-value)
   )
 
-
-
 (define (prime? n)
   (define (smallest-divisor n) (find-divisor n 2))
-(define (square x) (* x x))
+  (define (square x) (* x x))
   (define (divides? a b) (= (remainder b a) 0))
   (define (find-divisor n test-divisor)
     (cond ((> (square test-divisor) n) n)
           ((divides? test-divisor n) test-divisor)
           (else (find-divisor n (+ test-divisor 1)))))
   (= n (smallest-divisor n)))
+
+
+(define (square x) (* x x))
+
+(filtered-accumulate prime? + 0 square 1 next 7)
