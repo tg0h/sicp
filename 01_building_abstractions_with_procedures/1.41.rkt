63738e007068df33e9e7abcc8c5037f3e88d812c
tim goh <timslgoh@gmail.com>
2024-11-28 14:26:26 +0800

    01_building_abstractions_with_procedures/1.41.rkt

--- 01_building_abstractions_with_procedures/1.41.rkt
+++ 01_building_abstractions_with_procedures/1.41.rkt
@@ -7,9 +7,15 @@
   )
 
 ;; (inc (inc 1) )
-;; ((double inc) 1) ; adds 2 to a
-(((double double) inc) 5)
-;; (((double (double double)) inc) 5)
+((double inc) 1) ; adds 2 to a
+((lambda (x) (inc (inc x))) 1)
+((double double) 1) ; adds 2 to a
+
+((lambda (x) (double (double x))) 1)
+
+
+;; (((double double) inc) 5)
+(((double (double double)) inc) 5)
 ;; ((double (double (double inc))) 5)
 
 (((lambda (x) ((double double) ((double double) x))) inc) 5)
