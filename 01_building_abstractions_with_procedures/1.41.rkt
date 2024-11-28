ddf2b701664765c4757d01d4de79685ec61c5b32
tim goh <timslgoh@gmail.com>
2024-11-28 14:26:30 +0800

    01_building_abstractions_with_procedures/1.41.rkt

--- 01_building_abstractions_with_procedures/1.41.rkt
+++ 01_building_abstractions_with_procedures/1.41.rkt
@@ -10,7 +10,6 @@
 ((double inc) 1) ; adds 2 to a
 ((lambda (x) (inc (inc x))) 1)
 ((double double) 1) ; adds 2 to a
-
 ((lambda (x) (double (double x))) 1)
 
 
@@ -18,16 +17,16 @@
 (((double (double double)) inc) 5)
 ;; ((double (double (double inc))) 5)
 
-(((lambda (x) ((double double) ((double double) x))) inc) 5)
-
-(((lambda (x) ((lambda (x) (double (double x)))
-               ((lambda (x) (double (double x))) x))) inc) 5)
-
-(((lambda (x) ((lambda (x) (double (double x)))
-               (double (double x)))) inc) 5)
-
-(((lambda (x) (double (double (double (double x))))) inc) 5)
-
-((double (double (double (double inc)))) 5)
-
+;; (((lambda (x) ((double double) ((double double) x))) inc) 5)
+;;
+;; (((lambda (x) ((lambda (x) (double (double x)))
+;;                ((lambda (x) (double (double x))) x))) inc) 5)
+;;
+;; (((lambda (x) ((lambda (x) (double (double x)))
+;;                (double (double x)))) inc) 5)
+;;
+;; (((lambda (x) (double (double (double (double x))))) inc) 5)
+;;
+;; ((double (double (double (double inc)))) 5)
+;;
 ;; https://github.com/mngu2382/sicp/blob/master/fragments/Ex1-41.scm
