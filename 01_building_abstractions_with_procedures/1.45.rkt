8a53e60ba171592525eb4a1b31e5c5df2ce5c366
tim goh <timslgoh@gmail.com>
2024-11-29 14:24:05 +0800

    01_building_abstractions_with_procedures/1.45.rkt

--- 01_building_abstractions_with_procedures/1.45.rkt
+++ 01_building_abstractions_with_procedures/1.45.rkt
@@ -25,32 +25,41 @@
 
 
 (define (average x y) (/ (+ x y) 2))
+;; average
 (define (average-damp f)
-  (lambda(x) (average x f(x)))
-  )
+  (lambda(x) (average x (f x))))
+
 
 
 (define (sqrt-cyclic a) (fixed-point (lambda (x) (/ a x)) 1.0))
-(sqrt-cyclic 2)
+;; (sqrt-cyclic 2)
 
-
-
-(define (cube-root a) (fixed-point (lambda (x) (/ a (x * x)) 1.0)))
 
 (define (fixed-point-transform f transform)
   (fixed-point (transform f) 1.0)
   )
 
-(define (sqrt-f a) (lambda(x) (/ a  x)))
+;; (define (sqrt a)
+;;   (fixed-point-transform (lambda (x) (/ a x)) average-damp)
+;;   )
 
-;; (define (sqrt-damp a) (fixed-point-transform f ) 1.0))
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(sqrt 2)
+
+
+
+
+(define (cube-root a) (fixed-point (lambda (x) (/ a (x * x)) 1.0)))
+
 
 
 
 (define (root-n a n)
   (fixed-point (lambda (x) (neg-power a (- n 1))) 1.0)
   )
-
 
 (define (power a n) (if (= n 1) a (* a (power a (- n 1)))))
 (define (neg-power a n) (/ 1 (power a n)))
@@ -71,7 +80,7 @@
   (iter n)
   )
 
-(root-n 2 2) ; square root of 2
+;; (root-n 2 2) ; square root of 2
 ;; (root-n 2 3) ; cube root of 2
 ;; (root-n 2 2) ; 4th root of 2
 
