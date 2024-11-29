ea73205443f367b9f1fe085b1c398afa9dbfdba7
tim goh <timslgoh@gmail.com>
2024-11-29 12:15:44 +0800

    01_building_abstractions_with_procedures/1.44.rkt

--- 01_building_abstractions_with_procedures/1.44.rkt
+++ 01_building_abstractions_with_procedures/1.44.rkt
@@ -18,5 +18,12 @@
 (define dx 0.0001)
 
 (define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)
+    )
+  )
 
+(define (n-fold-smooth f n)
+  ;; (repeated (smooth f) n)
+  ((repeated smooth n) f)
   )
