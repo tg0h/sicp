9063fa46b5fe5af976b217f0b898a872e1a36c84
tim goh <timslgoh@gmail.com>
2024-11-29 11:24:31 +0800

    01_building_abstractions_with_procedures/1.41.rkt

--- 01_building_abstractions_with_procedures/1.41.rkt
+++ 01_building_abstractions_with_procedures/1.41.rkt
@@ -13,26 +13,10 @@
 ;; (((double double) inc) 1) ; adds 2 to a
 (((double double) inc)  0) ; adds 2 to a
 (((double (double double)) inc)  0) ; adds 2 to a
-((double (double (double inc)))  0) ; adds 2 to a
-;; ((lambda (x) (double (double x))) 1)
 
 ((double (double (double inc))) 5)
 
 
-;; (((double double) inc) 5)
-;; (((double (double double)) inc) 5)
 ((double (double (double inc))) 0)
 
-;; (((lambda (x) ((double double) ((double double) x))) inc) 5)
-;;
-;; (((lambda (x) ((lambda (x) (double (double x)))
-;;                ((lambda (x) (double (double x))) x))) inc) 5)
-;;
-;; (((lambda (x) ((lambda (x) (double (double x)))
-;;                (double (double x)))) inc) 5)
-;;
-;; (((lambda (x) (double (double (double (double x))))) inc) 5)
-;;
-;; ((double (double (double (double inc)))) 5)
-;;
 ;; https://github.com/mngu2382/sicp/blob/master/fragments/Ex1-41.scm
