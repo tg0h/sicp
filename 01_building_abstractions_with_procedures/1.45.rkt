94f04e4afa526e89d58f7bcddb34b7b5ee5cdf98
tim goh <timslgoh@gmail.com>
2024-11-29 13:37:23 +0800

    01_building_abstractions_with_procedures/1.45.rkt

--- 01_building_abstractions_with_procedures/1.45.rkt
+++ 01_building_abstractions_with_procedures/1.45.rkt
@@ -29,13 +29,20 @@
 ;; (fixed-point (lambda (x) (/ 1 x) ) 1.1)
 
 (define (sqrt a) (fixed-point (lambda (x) (/ a x)) 1.0))
+(sqrt 2)
 
-(define (power a n)
-  (if (= n 1) a (* a (power a (- n 1)))
-      )
+(define (root-n a n)
+  (fixed-point (lambda (x) (neg-power a (- n 1))) 1.0)
   )
 
-(power 2 5)
+
+(define (power a n) (if (= n 1) a (* a (power a (- n 1)))))
+(define (neg-power a n) (/ 1 (power a n)))
+
+;; (root-n 2 2) ; square root of 2
+;; (root-n 2 3) ; cube root of 2
+(root-n 2 4) ; 4th root of 2
+
 
 ;; (define (root-n a)
 ;;   lambda (x) (/ a ()
