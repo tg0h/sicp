5e30f8dd71ccf51e2c06f6ac270953d4df76743d
tim goh <timslgoh@gmail.com>
2024-11-29 12:07:34 +0800

    01_building_abstractions_with_procedures/1.44.rkt

---
+++ 01_building_abstractions_with_procedures/1.44.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (compose f g)
+  (lambda (x) (f (g x)))
+  )
+
+(define (repeated f n)
+  (define (iter i)
+    (if (= i 1)
+        f
+        (compose f (iter (- i 1)))
+        )
+    )
+  (iter n)
+  )
