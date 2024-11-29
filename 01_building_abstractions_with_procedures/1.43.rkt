7aa4f63199e646441fcc6e1a24024cd2624ad7de
tim goh <timslgoh@gmail.com>
2024-11-29 11:49:25 +0800

    01_building_abstractions_with_procedures/1.43.rkt

--- 01_building_abstractions_with_procedures/1.43.rkt
+++ 01_building_abstractions_with_procedures/1.43.rkt
@@ -2,11 +2,25 @@
 
 (define (square x) (* x x))
 
-(define (repeated f n)
+(define (repeated-wrong f n)
   (define (iter i)
     (if (= i 1)
         f
         (f (iter (- i 1)))
+        )
+    )
+  (iter n)
+  )
+
+(define (compose f g)
+  (lambda (x) (f (g x)))
+  )
+
+(define (repeated f n)
+  (define (iter i)
+    (if (= i 1)
+        f
+        (compose f (iter (- i 1)))
         )
     )
   (iter n)
