917688e472c09f7de3db2e14c28b80f05fb37638
tim goh <timslgoh@gmail.com>
2024-12-01 15:11:33 +0800

    01_building_abstractions_with_procedures/1.46.rkt

--- 01_building_abstractions_with_procedures/1.46.rkt
+++ 01_building_abstractions_with_procedures/1.46.rkt
@@ -11,3 +11,8 @@
     (iter guess)
     )
   )
+
+(define (average x y) (/ (+ x y) 2))
+(define (improve guess x) (average guess (/ x guess)))
+
+(define (good-enough? guess x) (< (abs (- (square guess) x)) 0.001))
