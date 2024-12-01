4a78bfd0777ef6f2caab5834e1bc3c5b50df1257
tim goh <timslgoh@gmail.com>
2024-12-01 15:32:02 +0800

    01_building_abstractions_with_procedures/1.46.rkt

--- 01_building_abstractions_with_procedures/1.46.rkt
+++ 01_building_abstractions_with_procedures/1.46.rkt
@@ -28,4 +28,18 @@
 
 ((iterative-improve good-enough-sqrt-2? improve-guess-sqrt-2) 1.0)
 
+(define fixed-point-good-enough? 
+  (lambda (guess) )
+  )
+
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+(define (close-enough? v1 v2) (< (abs (- v1 v2))
+tolerance)) (define (try guess)
+(let ((next (f guess)))
+(if (close-enough? guess next)
+next
+          (try next))))
+  (try first-guess))
 
