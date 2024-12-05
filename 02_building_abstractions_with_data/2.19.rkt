862f6eab8f15a39682d270741b8b50ff31e01eca
tim goh <timslgoh@gmail.com>
2024-12-05 15:15:26 +0800

    02_building_abstractions_with_data/2.19.rkt

--- 02_building_abstractions_with_data/2.19.rkt
+++ 02_building_abstractions_with_data/2.19.rkt
@@ -13,7 +13,13 @@
                      kinds-of-coins)))))
 
 (define (first-denomination kinds-of-coins)
-  (cond ((= kinds-of-coins 1) 1) ((= kinds-of-coins 2) 5)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+(count-change 100)
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
