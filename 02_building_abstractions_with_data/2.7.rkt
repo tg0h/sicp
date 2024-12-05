b6d1d2b0b67b449a4fbd2b9f02d975e393c397a6
tim goh <timslgoh@gmail.com>
2024-12-05 11:37:07 +0800

    02_building_abstractions_with_data/2.7.rkt

--- 02_building_abstractions_with_data/2.7.rkt
+++ 02_building_abstractions_with_data/2.7.rkt
@@ -23,3 +23,7 @@
 
 (define (lower-bound x) car x)
 (define (upper-bound x) cdr x)
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
