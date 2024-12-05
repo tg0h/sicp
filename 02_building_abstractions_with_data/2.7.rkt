31791a99b9e10b50e30a9bb5c5d8e252300097ee
tim goh <timslgoh@gmail.com>
2024-12-05 11:22:27 +0800

    02_building_abstractions_with_data/2.7.rkt

--- 02_building_abstractions_with_data/2.7.rkt
+++ 02_building_abstractions_with_data/2.7.rkt
@@ -11,3 +11,10 @@
         (p4 (* (upper-bound x) (upper-bound y))))
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
+
+
+(define (div-interval x y) 
+  (mul-interval
+                            x
+                            (make-interval (/ 1.0 (upper-bound y))
+                                           (/ 1.0 (lower-bound y)))))
