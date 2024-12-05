6a0ca7dc8d26616912874a0efc29f9852f635806
tim goh <timslgoh@gmail.com>
2024-12-05 12:59:34 +0800

    02_building_abstractions_with_data/2.12.rkt

--- 02_building_abstractions_with_data/2.12.rkt
+++ 02_building_abstractions_with_data/2.12.rkt
@@ -6,8 +6,13 @@
 
 (define (make-center-width c w)
   (make-interval (- c w) (+ c w)))
+
 (define (center i)
   (/ (+ (lower-bound i) (upper-bound i)) 2))
 (define (width i)
   (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+(define (make-center-percent c p)
+  (make-interval (- c (* p c)) (+ c (* p c))))
 
