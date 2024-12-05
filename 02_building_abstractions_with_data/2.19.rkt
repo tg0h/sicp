7bf758bb918448cd3f8ec1cf528d89e82d6891c1
tim goh <timslgoh@gmail.com>
2024-12-05 15:31:05 +0800

    02_building_abstractions_with_data/2.19.rkt

--- 02_building_abstractions_with_data/2.19.rkt
+++ 02_building_abstractions_with_data/2.19.rkt
@@ -1,7 +1,6 @@
 #lang sicp
 
-(define (count-change amount)
-  (cc amount 5))
+(define (count-change amount) (cc amount 5))
 
 (define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
@@ -22,6 +21,7 @@
 (count-change 100)
 
 (define us-coins (list 50 25 10 5 1))
+(define _us-coins (list 25 1 50 10 5 ))
 (define uk-coins (list 100 50 20 10 5 2 1 0.5))
 
 
@@ -30,10 +30,18 @@
         ((or (< amount 0) (no-more? coin-values)) 0)
         (else
          (+ (_cc amount
-                (except-first-denomination
-                 coin-values))
+                 (except-first-denomination
+                  coin-values))
             (_cc (- amount
-                   (first-denomination
-                    coin-values))
-                coin-values)))))
+                    (_first-denomination
+                     coin-values))
+                 coin-values)))))
 
+
+
+(define (_first-denomination l) (car l))
+(define (except-first-denomination l) (cdr l))
+(define (no-more? l) (= (length l) 0))
+
+(_cc 100 us-coins)
+(_cc 100 _us-coins)
