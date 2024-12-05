8fd1c1fa5befb8196997df9a086f3c30599c50ac
tim goh <timslgoh@gmail.com>
2024-12-05 13:25:46 +0800

    02_building_abstractions_with_data/2.14.rkt

---
+++ 02_building_abstractions_with_data/2.14.rkt
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+
+(define (make-center-percent c p)
+  (make-interval (- c (* p c)) (+ c (* p c))))
+
+
+(define (percent i)
+  (/ (width i) (center i))
+  )
+
+(define int (make-interval 1 2))
+
+(define int2 (make-center-width 1.5 0.5))
+
+(width int)
+(center int)
+(percent int)
+(define a (make-center-percent 1 0.9))
+(define b (make-center-percent 2 0.4))
+(define c (mul-interval a b))
+(percent c)
+
+
