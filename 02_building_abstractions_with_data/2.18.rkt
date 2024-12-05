00838ed3cae73785865c40abfe7da782bde62dbb
tim goh <timslgoh@gmail.com>
2024-12-05 14:40:22 +0800

    02_building_abstractions_with_data/2.18.rkt

--- 02_building_abstractions_with_data/2.18.rkt
+++ 02_building_abstractions_with_data/2.18.rkt
@@ -1,3 +1,14 @@
 #lang sicp
 
 (reverse (list 1 4 9 16 25))
+
+(define (pop l)
+  (if (null? (cdr l))
+      (car nil)
+      (cons (car l) (pop (cdr l)))
+      )
+  )
+
+;; (define (reverse l)
+;;   if (null? l)
+;;   )
