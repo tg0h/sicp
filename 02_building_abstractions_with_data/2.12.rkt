6975edacb707e1488e854131d3e711c768339184
tim goh <timslgoh@gmail.com>
2024-12-05 13:07:06 +0800

    02_building_abstractions_with_data/2.12.rkt

--- 02_building_abstractions_with_data/2.12.rkt
+++ 02_building_abstractions_with_data/2.12.rkt
@@ -1,5 +1,12 @@
 #lang sicp
 
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
 (define (make-interval a b) (cons a b))
 (define (lower-bound x) (car x))
 (define (upper-bound x) (cdr x))
@@ -16,3 +23,18 @@
 (define (make-center-percent c p)
   (make-interval (- c (* p c)) (+ c (* p c))))
 
+
+(define (percent i)
+  (/ (width i) (center i))
+  )
+
+(define int (make-interval 1 2))
+
+(define int2 (make-center-width 1.5 0.5))
+
+(width int)
+(center int)
+(percent int)
+(define a (make-center-percent 1 0.01))
+(define b (make-center-percent 2 0.01))
+()
