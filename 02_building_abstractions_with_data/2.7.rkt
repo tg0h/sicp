1642f06171d663de7ab21254f6d015667fae9c9b
tim goh <timslgoh@gmail.com>
2024-12-05 12:54:11 +0800

    02_building_abstractions_with_data/2.7.rkt

--- 02_building_abstractions_with_data/2.7.rkt
+++ 02_building_abstractions_with_data/2.7.rkt
@@ -13,11 +13,19 @@
                    (max p1 p2 p3 p4))))
 
 
+(define (contains-zero x)
+  (and
+   (< (lower-bound x) 0)
+   (> (upper-bound x) 0)))
+
 (define (div-interval x y)
-  (mul-interval
-   x
-   (make-interval (/ 1.0 (upper-bound y))
-                  (/ 1.0 (lower-bound y)))))
+  ( if (contains-zero y) (error "division by zero")
+       (mul-interval
+        x
+        (make-interval (/ 1.0 (upper-bound y))
+                       (/ 1.0 (lower-bound y))))
+       )
+  )
 
 (define (make-interval a b) (cons a b))
 
@@ -33,12 +41,27 @@
   )
 
 (define a (make-interval 1 2))
-(define b (make-interval 3 4))
+(define b (make-interval 3 8))
+(define c (make-interval -1 8))
 ( define (print-interval i)
    (display (lower-bound i))
    (display (upper-bound i))
    )
 
 ;; (add-interval a b)
-(print-interval a)
+(width a)
+(width b)
+;; (sub-interval a b)
+;; (width (add-interval a b))
+;; (width (sub-interval a b))
+
+(mul-interval a b)
+(width(mul-interval a b))
+(div-interval a b)
+(width(div-interval a b))
+(contains-zero c)
+(div-interval a c)
+
+
+;; (print-interval a)
 ;; (lower-bound a )
