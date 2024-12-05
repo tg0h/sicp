5fa20efac1eab8bb4c6ec536b0b96e92da8ca48a
tim goh <timslgoh@gmail.com>
2024-12-05 15:16:02 +0800

    02_building_abstractions_with_data/2.19.rkt

--- 02_building_abstractions_with_data/2.19.rkt
+++ 02_building_abstractions_with_data/2.19.rkt
@@ -23,3 +23,17 @@
 
 (define us-coins (list 50 25 10 5 1))
 (define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+
+(define (_cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (_cc amount
+                (except-first-denomination
+                 coin-values))
+            (_cc (- amount
+                   (first-denomination
+                    coin-values))
+                coin-values)))))
+
