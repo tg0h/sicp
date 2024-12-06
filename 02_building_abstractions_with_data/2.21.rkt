9312a81326a925a55a00c14960bf9b51decb08f5
tim goh <timslgoh@gmail.com>
2024-12-06 08:03:51 +0800

    02_building_abstractions_with_data/2.21.rkt

--- 02_building_abstractions_with_data/2.21.rkt
+++ 02_building_abstractions_with_data/2.21.rkt
@@ -6,10 +6,8 @@
       (cons (* (car items) (car items)) (square-list (cdr items))))
   )
 
-
 (define (_square-list items)
   (map (lambda (x) (* x x )) items))
-
 
 (square-list (list 1 2 3 4))
 (_square-list (list 1 2 3 4))
