fdd83dffb6e2ca9b42f5c24fa1e5e09898b138e5
tim goh <timslgoh@gmail.com>
2024-12-06 13:34:25 +0800

    02_building_abstractions_with_data/2.27.rkt

--- 02_building_abstractions_with_data/2.27.rkt
+++ 02_building_abstractions_with_data/2.27.rkt
@@ -24,17 +24,52 @@
       )
   )
 
+
 (define (deep-reverse l)
+  ;; (display l)
+  ;; (newline)
+  (display "l:")(display l)
+  (newline)
+  (display "cdr l:")(display (cdr l))
+  (newline)
+  (display "pair? l:")(display (pair? l))
+  (newline)
+  (display "last-pair l:")(display (last-pair l))
+  (newline)
+  (display "pop l:")(display (pop l))
+  (newline)
+  (newline)
   (cond
-    ((null? (cdr l)))
-    ((pair? l) deep-reverse l)
-    ((cons (last-pair l) (reverse (pop  l))))
-    )
-  )
+    ((null? (cdr l) )
+     (display ">null   ")(newline)
+     nil
+               )
+    ((not (pair? (cdr l)))
+(display ">notpair")(newline)
+(cons (deep-reverse(cdr l)) (deep-reverse (pop l)))
+                                                                                               )
+                                                                                              (else
+(display ">else")(newline)
+     (cons (deep-reverse (cdr l)) (deep-reverse (pop l)))
+)
+)
+)
 
 ;; (reverse (list 1 4 9 16 25))
 
-(define x (list (list 1 2) (list 3 4)))
+;; (define x (list (list 1 2) (list 3 4)))
+(define x (list 1 (list 2 3)))
 x
-(last-pair x)
-(last-pair(last-pair x))
+(newline)
+(newline)
+(newline)
+(deep-reverse x)
+;; (last-pair (list 3))
+;; (last-pair x)
+;; (pop x)
+;; (pair?(last-pair x))
+;; (last-pair(last-pair x))
+
+
+
+;; (cdr (list 1 2 3))
