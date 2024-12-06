1e410f29648320cfd382324a679d2734cb10030d
tim goh <timslgoh@gmail.com>
2024-12-06 08:08:13 +0800

    02_building_abstractions_with_data/2.22.rkt

--- 02_building_abstractions_with_data/2.22.rkt
+++ 02_building_abstractions_with_data/2.22.rkt
@@ -6,8 +6,19 @@
       (cons (* (car items) (car items)) (square-list (cdr items))))
   )
 
-(define (_square-list items)
-  (map (lambda (x) (* x x )) items))
+(define (_square-list items) (map (lambda (x) (* x x )) items))
 
 (square-list (list 1 2 3 4))
 (_square-list (list 1 2 3 4))
+
+(define (square x )(* x x ))
+
+(define (ssquare-list items)
+  (define (iter things answer)
+    (if (null? things) answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+(ssquare-list (list 1 2 3 4))
