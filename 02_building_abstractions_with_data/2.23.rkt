3ae0f58a323dbd1409d032650ac75005111936ba
tim goh <timslgoh@gmail.com>
2024-12-06 09:09:37 +0800

    02_building_abstractions_with_data/2.23.rkt

--- 02_building_abstractions_with_data/2.23.rkt
+++ 02_building_abstractions_with_data/2.23.rkt
@@ -1,13 +1,23 @@
 #lang sicp
 
-(define (for-each f l)
+(define (_for-each f l)
   (if (not (null? l))
-      (f (car l))
-      (for-each f (cdr l))
+      (begin
+        (display l)
+        (f (car l))
+        (newline)
+        (_for-each f (cdr l))
+        )
+      (display "else")
       )
   )
 
-(for-each
- (lambda (x) (newline)
+
+;; (if (not(null? (list 7))) (newline) (display "tim"))
+
+(_for-each
+ (lambda (x)
+   (display "printing:")
    (display x))
  (list 57 321 88))
+
