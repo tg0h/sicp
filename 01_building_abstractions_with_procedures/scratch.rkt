558a5fa9f5bb38dcca609f86c8008e49a2eec1cc
tim goh <timslgoh@gmail.com>
2024-12-07 10:25:32 +0800

    01_building_abstractions_with_procedures/scratch.rkt

--- 01_building_abstractions_with_procedures/scratch.rkt
+++ 01_building_abstractions_with_procedures/scratch.rkt
@@ -1,3 +1,4 @@
 #lang sicp
 
 (and "1" 2)
+(append 1 (list 1 2 ))
--- 02_building_abstractions_with_data/2.28.rkt
+++ 02_building_abstractions_with_data/2.28.rkt
@@ -1,18 +1,26 @@
 #lang sicp
 
-(define (fringe x)
-  (define (iter result)
-    (cond
-      ((null? x) nil)
-      ((not(pair? x) nil) x)
-      (else
-       (append (fringe (car x)) (fringe (cdr x)))
-       )
-      )
-    )
-  (iter (list nil))
-  )
+;; (define (fringe x)
+;;   (define (iter result)
+;;     (cond
+;;       ((null? x) nil)
+;;       ((not(pair? x) nil) x)
+;;       (else
+;;        (append (fringe (car x)) (fringe (cdr x)))
+;;        )
+;;       )
+;;     )
+;;   (iter (list nil))
+;;   )
 
 (define x (list (list 1 2) (list 3 4)))
+
+
+(define (fringe x)
+  (if (pair? x)
+      (append (fringe (car x))
+              (list (fringe (cdr x))))
+      x))
+
 
 (fringe x)
