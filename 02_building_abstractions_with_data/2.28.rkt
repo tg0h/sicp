b3cd3f524c830010b13bbceabce9ff9e3331fcd4
tim goh <timslgoh@gmail.com>
2024-12-09 13:38:56 +0800

    02_building_abstractions_with_data/2.28.rkt

--- 02_building_abstractions_with_data/2.28.rkt
+++ 02_building_abstractions_with_data/2.28.rkt
@@ -24,3 +24,27 @@
         (else (list x))))
 
 (fringe x)
+
+
+;; (define (fringe x)
+;;   ;; (display x)
+;;   ;; (newline)
+;;   (cond
+;;     ((null? x) x)
+;;     ((not (pair? x)) (list x))
+;;     ((pair? x) (append (fringe (car x)) (fringe (cdr x))))
+;;     )
+;;   )
+;;
+;; ;; (append (deep-reverse (cdr x))
+;; ;;         (list (deep-reverse (car x))))
+;;
+;;
+;; (define x
+;;   (list 1
+;;         (list 2
+;;               (list 3)
+;;               )
+;;         )
+;;   )
+;;
