7b4b3c2ef301c550287f4cf20281d6201732f8e4
tim goh <timslgoh@gmail.com>
2024-12-11 07:33:31 +0800

    02_building_abstractions_with_data/2.37.rkt

--- 02_building_abstractions_with_data/2.37.rkt
+++ 02_building_abstractions_with_data/2.37.rkt
@@ -1,12 +1,20 @@
 #lang sicp
 
-(define v list (1 2))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+(define v (list 1 2))
 
 (define w 
-  list (
-    list (1 2)
-    list (3 4)
-  )
+  (list 
+    (list 1 2)
+    (list 3 4))
   )
 
-(map * v w)
+;; (map * v w)
+v
+w
+
+(accumulate + 0 (map * v w))
