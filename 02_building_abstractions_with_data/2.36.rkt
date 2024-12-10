65978f002696c164c212bf280c7991e1875b7f33
tim goh <timslgoh@gmail.com>
2024-12-10 14:39:44 +0800

    02_building_abstractions_with_data/2.36.rkt

--- 02_building_abstractions_with_data/2.36.rkt
+++ 02_building_abstractions_with_data/2.36.rkt
@@ -10,15 +10,16 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       nil
-      (cons (accumulate op init (map (lambda (x) car x) seqs))
-            (accumulate-n op init (map (lambda (x) cdr x) seqs)))
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))
       )
+  )
 
-  (define x (list
-             (list 1 2 3)
-             (list 4 5 6)
-             (list 7 8 9)
-             )
-    )
+(define x (list
+           (list 1 2 3)
+           (list 4 5 6)
+           (list 7 8 9)
+           )
+  )
 
-  (accumulate-n + 0 x)
+(accumulate-n + 0 x)
