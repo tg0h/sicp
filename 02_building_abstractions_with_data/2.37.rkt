2fc2193991564823050516b6d6e39ac2e102bdc2
tim goh <timslgoh@gmail.com>
2024-12-11 08:21:03 +0800

    02_building_abstractions_with_data/2.37.rkt

--- 02_building_abstractions_with_data/2.37.rkt
+++ 02_building_abstractions_with_data/2.37.rkt
@@ -28,3 +28,14 @@
   )
 
 (matrix-*-vector m v)
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))
+      )
+  )
+(define (transpose mat)
+
+  )
