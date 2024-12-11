e383ecdce69dba489c5a1a2067f293f1f02fd106
tim goh <timslgoh@gmail.com>
2024-12-11 10:09:24 +0800

    02_building_abstractions_with_data/2.38.rkt

--- 02_building_abstractions_with_data/2.38.rkt
+++ 02_building_abstractions_with_data/2.38.rkt
@@ -1,6 +1,5 @@
 #lang sicp
 
-
 (define (fold-right op initial sequence)
   (if (null? sequence)
       initial
@@ -13,3 +12,9 @@
         (iter (op result (car rest))
               (cdr rest))))
   (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+(fold-left / 1 (list 1 2 3))
+
+(fold-right list nil (list 1 2 3))
+(fold-left list nil (list 1 2 3))
