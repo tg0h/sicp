876599928c8dd4f3a315d6089f03bc1d3fb85038
tim goh <timslgoh@gmail.com>
2024-12-12 08:54:48 +0800

    02_building_abstractions_with_data/2.40.rkt

--- 02_building_abstractions_with_data/2.40.rkt
+++ 02_building_abstractions_with_data/2.40.rkt
@@ -1,6 +1,26 @@
 #lang sicp
 
+(define (enumerate-interval low high)
+  (if (> low high) nil (cons low (enumerate-interval (+ low 1) high)))
+  )
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
 
 (define (unique-pairs n)
-   
+  (accumulate
+   append
+   nil
+   (map (lambda (i)
+          (map (lambda (j)
+                 (list i j))
+               (enumerate-interval 1 (- i 1))
+               ))
+        (enumerate-interval 1 n)
+        )
+   )
   )
+
+(unique-pairs 2)
