56207807712d2a20ad13602996230a8c9526717d
tim goh <timslgoh@gmail.com>
2024-12-13 14:27:25 +0800

    02_building_abstractions_with_data/2.42.rkt

--- 02_building_abstractions_with_data/2.42.rkt
+++ 02_building_abstractions_with_data/2.42.rkt
@@ -79,18 +79,21 @@
 ;; (define aboard (list (list 1 2) ))
 ;; (adjoin-position 2 2 aboard)
 
+(define (flatmap proc seq) (accumulate append nil (map proc seq)))
+(define (enumerate-interval low high)
+  (if (> low high) nil (cons low (enumerate-interval (+ low 1) high)))
+  )
+
+(define empty-board (list(list nil)))
 (define (queens board-size)
   (define (queen-cols k)
     (if (= k 0)
         (list empty-board)
-        (filter
-         (lambda (positions)
-           (safe? k positions))
-         (flatmap
-          (lambda (rest-of-queens)
-            (map (lambda (new-row)
-                   (adjoin-position
-                    new-row k rest-of-queens))
-                 (enumerate-interval 1 board-size)))
-          (queen-cols (- k 1))))))
+        (filter (lambda (positions) (safe? k positions))
+                (flatmap (lambda (rest-of-queens) (map (lambda (new-row)
+                                                         (adjoin-position new-row k rest-of-queens))
+                                                       (enumerate-interval 1 board-size)))
+                         (queen-cols (- k 1))))))
   (queen-cols board-size))
+
+(queens 4)
