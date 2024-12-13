164b299fabd75d1ff30ad38716513f1d56a7de16
tim goh <timslgoh@gmail.com>
2024-12-13 13:45:21 +0800

    02_building_abstractions_with_data/2.42.rkt

---
+++ 02_building_abstractions_with_data/2.42.rkt
@@ -0,0 +1,50 @@
+#lang sicp
+
+(define (horizontal? point-a point-b) (= (cadr point-a) (cadr point-b)))
+(define (diagonal? point-a point-b)
+  (define (abs x) (cond ((> x 0) x) ((= x 0) 0) ((< x 0) (- x))))
+  (let
+      (
+       (x-point-a (car point-a))
+       (y-point-a (cadr point-a))
+       (x-point-b (car point-b))
+       (y-point-b (cadr point-b))
+       )
+    (= (abs (- y-point-b y-point-a)) (abs(- x-point-b x-point-a)))
+    )
+  )
+(define (is-point-safe? point-a point-b)
+  (and
+   (not (horizontal? point-a point-b))
+   (not (diagonal? point-a point-b))
+   )
+  )
+
+(define (check-point-safe-with-positions test-point positions)
+  (accumulate
+   (lambda (point is-rest-of-points-safe)
+     (and (is-point-safe? point test-point) is-rest-of-points-safe)
+     )
+   #t positions)
+  )
+
+(define (safe? k positions)
+  (filter (lambda (position)
+            (let
+                (
+                 (col-k-point (car (filter (lambda (point)
+                                             (= (car point) k)
+                                             ) position
+                                               )))
+                 (test-points (filter (lambda (point)
+                                        (not(= (car point) k))
+                                        ) position
+                                          ))
+                 )
+              ;; (display col-k-point) (display "test against->") (display test-points)
+              ;; (newline)
+              (check-point-safe-with-positions col-k-point test-points)
+              )
+            ) positions
+              )
+  )
