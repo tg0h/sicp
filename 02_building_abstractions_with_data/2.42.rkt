9a4af0cdf26a1236e5b8cb17a71d38784eb63c60
tim goh <timslgoh@gmail.com>
2024-12-13 14:18:10 +0800

    02_building_abstractions_with_data/2.42.rkt

--- 02_building_abstractions_with_data/2.42.rkt
+++ 02_building_abstractions_with_data/2.42.rkt
@@ -78,3 +78,19 @@
 
 ;; (define aboard (list (list 1 2) ))
 ;; (adjoin-position 2 2 aboard)
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions)
+           (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                    new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
