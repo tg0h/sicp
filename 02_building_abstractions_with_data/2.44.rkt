ba6cf8d35c250bf3a711540b35202fa51bd55b81
tim goh <timslgoh@gmail.com>
2024-12-14 13:44:43 +0800

    02_building_abstractions_with_data/2.44.rkt

---
+++ 02_building_abstractions_with_data/2.44.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+(#%require sicp-pict)
+(paint einstein)
+
+ (define (right-split painter n) 
+    (if (= n 0) 
+        painter 
+        (let ((smaller (right-split painter (- n 1)))) 
+          (beside painter (below smaller smaller))))) 
+  
+ (paint (right-split einstein 3))
+
