c9399b598881d1cbb9481dbf5a53a11ca6de3c29
tim goh <timslgoh@gmail.com>
2024-12-14 12:06:58 +0800

    02_building_abstractions_with_data/2.42.rkt

--- 02_building_abstractions_with_data/2.42.rkt
+++ 02_building_abstractions_with_data/2.42.rkt
@@ -35,7 +35,7 @@
   )
 
 (define (check-point-safe-with-positions test-point positions)
-  (display "check-point-safe-with-positions")(display positions)(newline)
+  ;; (display "check-point-safe-with-positions")(display positions)(newline)
   (accumulate
    (lambda (point is-rest-of-points-safe)
      (and (is-point-safe? point test-point) is-rest-of-points-safe)
@@ -44,19 +44,19 @@
   )
 
 (define (safe? k board)
-  (newline)
+  ;; (newline)
   (display "safe? ")(display k)(display " ")(display board)(newline)
   (let
       (
        (col-k-point (car (filter (lambda (point)
-                                   (display "k ->")(display k)(newline)
-                                   (display "point->")(display point)(newline)
-                                   (display "k point car->")(display (car point))(newline)
+                                   ;; (display "k ->")(display k)(newline)
+                                   ;; (display "point->")(display point)(newline)
+                                   ;; (display "k point car->")(display (car point))(newline)
                                    (= (car point) k)
                                    ) board
                                      )))
        (test-points (filter (lambda (point)
-                              (display "test point->")(display point)(newline)
+                              ;; (display "test point->")(display point)(newline)
                               (not (= (car point) k))
                               ) board
                                 ))
@@ -137,7 +137,7 @@
   (queen-cols board-size)
   )
 
-(queens 3)
+(queens 4)
 
 (newline)
 
