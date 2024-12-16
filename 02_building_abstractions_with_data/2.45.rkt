95c73784dbb3cebfcb6d1378a63831b3c78aa6f0
tim goh <timslgoh@gmail.com>
2024-12-16 14:20:31 +0800

    02_building_abstractions_with_data/2.45.rkt

---
+++ 02_building_abstractions_with_data/2.45.rkt
@@ -0,0 +1,46 @@
+#lang sicp
+(#%require sicp-pict)
+(paint einstein)
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(paint (right-split einstein 3))
+
+;; (paint (beside einstein einstein))
+;; (paint
+;;  (beside (beside einstein einstein)
+;;          (beside einstein einstein))
+;;  )
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
+
+(paint (up-split einstein 1))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right)) (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner)
+                  )))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+(define (_square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside quarter (flip-horiz quarter) )))
+      (below (flip-vert half) half))))
