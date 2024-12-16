a6a9436aa5203b79bd8c1ad56bc58b1542528cc0
tim goh <timslgoh@gmail.com>
2024-12-16 14:48:36 +0800

    02_building_abstractions_with_data/2.46.rkt

--- 02_building_abstractions_with_data/2.46.rkt
+++ 02_building_abstractions_with_data/2.46.rkt
@@ -4,3 +4,17 @@
 (define (make-vect x y)
   (cons x y)
   )
+
+(define (xcor-vect v)
+  (car v)
+  )
+
+
+(define (ycor-vect v)
+  (cdr v)
+  )
+
+(define v (make-vect 1 2))
+(xcor-vect v)
+(ycor-vect v)
+
