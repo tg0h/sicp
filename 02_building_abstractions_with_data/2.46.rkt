cad43a2fe8d8fece67d1a85dc5c38a4ede68a472
tim goh <timslgoh@gmail.com>
2024-12-16 14:51:34 +0800

    02_building_abstractions_with_data/2.46.rkt

--- 02_building_abstractions_with_data/2.46.rkt
+++ 02_building_abstractions_with_data/2.46.rkt
@@ -14,7 +14,31 @@
   (cdr v)
   )
 
-(define v (make-vect 1 2))
-(xcor-vect v)
-(ycor-vect v)
+(define v1 (make-vect 1 2))
+(define v2 (make-vect 3 4))
 
+(define (add-vect v1 v2)
+  (let
+      (
+       (x-v1 (xcor-vect v1))
+       (y-v1 (ycor-vect v1))
+       (x-v2 (xcor-vect v2))
+       (y-v2 (ycor-vect v2))
+       )
+    (make-vect (+ x-v1 x-v2) (+ y-v1 y-v2))
+    )
+  )
+
+(add-vect v1 v2)
+
+(define (sub-vect v1 v2)
+  (let
+      (
+       (x-v1 (xcor-vect v1))
+       (y-v1 (ycor-vect v1))
+       (x-v2 (xcor-vect v2))
+       (y-v2 (ycor-vect v2))
+       )
+    (make-vect (- x-v1 x-v2) (- y-v1 y-v2))
+    )
+  )
