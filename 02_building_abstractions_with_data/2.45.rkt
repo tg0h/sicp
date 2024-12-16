5ad474113ad4fbb112a6fa54a8f36ff6f8dae9c9
tim goh <timslgoh@gmail.com>
2024-12-16 14:26:51 +0800

    02_building_abstractions_with_data/2.45.rkt

--- 02_building_abstractions_with_data/2.45.rkt
+++ 02_building_abstractions_with_data/2.45.rkt
@@ -44,3 +44,16 @@
   (let ((quarter (corner-split painter n)))
     (let ((half (beside quarter (flip-horiz quarter) )))
       (below (flip-vert half) half))))
+
+(define (split op1 op2)
+  (lambda(painter n)
+    (define (iter n)
+      (if (= n 0) painter
+          ( let ((smaller (iter (- n 1))))
+             (op1 painter (op2 smaller smaller))
+             )
+          )
+      )
+    (iter n)
+    )
+  )
