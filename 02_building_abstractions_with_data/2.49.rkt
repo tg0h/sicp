17aeec402e14f637be534226cb0e5698d3b204e1
tim goh <timslgoh@gmail.com>
2024-12-17 10:00:23 +0800

    02_building_abstractions_with_data/2.49.rkt

--- 02_building_abstractions_with_data/2.49.rkt
+++ 02_building_abstractions_with_data/2.49.rkt
@@ -84,8 +84,10 @@
  )
 
 ; 2.49.c
-; draw an x
+; draw a diamond
 (list
- (make-segment (make-vect 0 0) (make-vect 1 1))
- (make-segment (make-vect 1 0) (make-vect 0 1))
+ (make-segment (make-vect 0.5 0) (make-vect 1 0.5))
+ (make-segment (make-vect 1 0.5) (make-vect 0.5 1))
+ (make-segment (make-vect 0.5 1) (make-vect 0 0.5))
+ (make-segment (make-vect 0 0.5) (make-vect 0.5 0))
  )
