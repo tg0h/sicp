d04fbc42a90d3af6a91be3b47a6ea61c58e5500d
tim goh <timslgoh@gmail.com>
2024-12-20 11:38:24 +0800

    02_building_abstractions_with_data/2.51.rkt

--- 02_building_abstractions_with_data/2.51.rkt
+++ 02_building_abstractions_with_data/2.51.rkt
@@ -17,7 +17,7 @@
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
 
-(paint (flip-horiz einstein))
+;; (paint (flip-horiz einstein))
 
 ; counter clockwise
 (define (rotate-180 painter)
@@ -26,7 +26,7 @@
                      (make-vect 0.0 1.0)
                      (make-vect 1.0 0.0)))
 
-(paint (rotate-180 einstein))
+;; (paint (rotate-180 einstein))
 
 (define (rotate-270 painter)
   (transform-painter painter
@@ -34,4 +34,19 @@
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
 
-(paint (rotate-270 einstein))
+;; (paint (rotate-270 einstein))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left (transform-painter
+                       painter1
+                       (make-vect 0.0 0.0)
+                       split-point
+                       (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter
+            painter2
+            split-point
+            (make-vect 1.0 0.0)
+            (make-vect 0.5 1.0))))
+      (lambda (frame) (paint-left frame) (paint-right frame)))))
