988a9d241f9209c5ca18e38c57e30e34d46b9084
tim goh <timslgoh@gmail.com>
2024-12-20 11:09:54 +0800

    02_building_abstractions_with_data/2.50.rkt

---
+++ 02_building_abstractions_with_data/2.50.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+(#%require sicp-pict)
+(paint einstein)
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+
+(paint (flip-vert einstein))
