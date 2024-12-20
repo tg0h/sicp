402e406aaaf130a2fffe7fd5f2d15fd99937ba2a
tim goh <timslgoh@gmail.com>
2024-12-20 11:20:33 +0800

    02_building_abstractions_with_data/2.50.rkt

--- 02_building_abstractions_with_data/2.50.rkt
+++ 02_building_abstractions_with_data/2.50.rkt
@@ -18,3 +18,13 @@
                      (make-vect 1.0 1.0)))
 
 (paint (flip-horiz einstein))
+
+
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(paint (rotate-180 einstein))
