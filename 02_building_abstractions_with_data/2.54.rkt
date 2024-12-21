7e2ce112078805d41165ac9e12286df22d30c560
tim goh <timslgoh@gmail.com>
2024-12-21 11:46:28 +0800

    02_building_abstractions_with_data/2.54.rkt

--- 02_building_abstractions_with_data/2.54.rkt
+++ 02_building_abstractions_with_data/2.54.rkt
@@ -7,9 +7,10 @@
   (cond
     ((and (not (pair? a)) (not (pair? b))) (eq? a b))
     ((and (pair? a) (pair? b)) (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
-    (else #f)
+    (else false)
     )
   )
 
 (equal? '(this is a list) '(this is a list))
+(equal? '(1 is a list) '(1 is a list))
 (equal? '(this is a list) '(this (is a) list))
