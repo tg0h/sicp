0b3ec16b0ae8e0bb4f15873e53ee5e41674e56b3
tim goh <timslgoh@gmail.com>
2024-12-21 11:44:51 +0800

    02_building_abstractions_with_data/2.54.rkt

--- 02_building_abstractions_with_data/2.54.rkt
+++ 02_building_abstractions_with_data/2.54.rkt
@@ -1,17 +1,15 @@
 #lang sicp
 
-(eq? 'a 'a)
-(eq? 1 2)
+;; (eq? 'a 'a)
+;; (eq? 1 2)
 
 (define (equal? a b)
   (cond
     ((and (not (pair? a)) (not (pair? b))) (eq? a b))
-    ((and (pair? a) (pair? b)) ((and (equal? (car a) (car b)) (equal? (cdr a) (cdr b)))))
+    ((and (pair? a) (pair? b)) (and (equal? (car a) (car b)) (equal? (cdr a) (cdr b))))
     (else #f)
     )
   )
 
-
 (equal? '(this is a list) '(this is a list))
-
 (equal? '(this is a list) '(this (is a) list))
