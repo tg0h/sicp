8a16925a84b7665411d2577cbbb691c490ac052c
tim goh <timslgoh@gmail.com>
2024-12-21 11:24:13 +0800

    02_building_abstractions_with_data/2.54.rkt

--- 02_building_abstractions_with_data/2.54.rkt
+++ 02_building_abstractions_with_data/2.54.rkt
@@ -3,29 +3,15 @@
 (eq? 'a 'a)
 (eq? 1 2)
 
-(define (abs x)
-  (cond
-    ((> x 0) x)
-    ((= x 0) 0)
-    (else #f)
-    )
-  )
-
 (define (equal? a b)
   (cond
     ((and (not (pair? a)) (not (pair? b))) (eq? a b))
     ((and (pair? a) (pair? b)) ((and (equal? (car a) (car b)) (equal? (cdr a) (cdr b)))))
     (else #f)
-
     )
   )
 
-(define (_equal? a b)
-  (cond (
-         ((and (pair? a)) (eq? a b))
-         ((and (pair? a)) (eq? a b))
-         (else a)
-         )
-        )
-  )
 
+(equal? '(this is a list) '(this is a list))
+
+(equal? '(this is a list) '(this (is a) list))
