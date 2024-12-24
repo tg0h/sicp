a4f39b1ff180698a59e7d805dca1611ba1c5abf8
tim goh <timslgoh@gmail.com>
2024-12-24 10:54:38 +0800

    02_building_abstractions_with_data/2.59.rkt

--- 02_building_abstractions_with_data/2.59.rkt
+++ 02_building_abstractions_with_data/2.59.rkt
@@ -9,16 +9,16 @@
 (define set2 (list 6 4 5))
 (define set3 (list 3 4 5))
 
-(element-of-set? 1 set1 )
-(element-of-set? 5 set1 )
+;; (element-of-set? 1 set1 )
+;; (element-of-set? 5 set1 )
 
 (define (adjoin-set x set)
   (if (element-of-set? x set)
       set
       (cons x set)))
 
-(adjoin-set 9 set1)
-(adjoin-set 1 set1)
+;; (adjoin-set 9 set1)
+;; (adjoin-set 1 set1)
 
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2)) '())
@@ -26,18 +26,22 @@
          (cons (car set1) (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
 
-(intersection-set set1 set2)
-(intersection-set set1 set3)
+;; (intersection-set set1 set2)
+;; (intersection-set set1 set3)
 
 (define (union-set set1 set2)
   (define (iter result _list)
     (cond
       ((null? _list) result)
-      ( (element-of-set? (car _list) result)
-        (iter result (cdr _list))
-        (iter (append (list (car list)) result) (cdr _list))
-        )
+      ((element-of-set? (car _list) result) 
+       (iter result (cdr _list)))
+      (else
+       (iter (append (list (car list)) result) (cdr _list))
+       )
       )
     )
   (iter '() (append set1 set2))
   )
+
+;; (union-set set1 set2)
+(append set1 set2)
