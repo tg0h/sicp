f29f6dc1af4d9e0bdbcf1a8d7704fe03809c9d93
tim goh <timslgoh@gmail.com>
2024-12-24 12:13:59 +0800

    02_building_abstractions_with_data/2.61.rkt

--- 02_building_abstractions_with_data/2.61.rkt
+++ 02_building_abstractions_with_data/2.61.rkt
@@ -33,30 +33,3 @@
 (adjoin-set 1 set1)
 (adjoin-set 999 set1)
 
-(define (intersection-set set1 set2)
-  (cond ((or (null? set1) (null? set2)) '())
-        ((element-of-set? (car set1) set2)
-         (cons (car set1) (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
-
-;; (intersection-set set1 set3)
-;; (intersection-set set1 set3)
-
-(define (union-set set1 set2)
-  (define (iter result _list)
-    (cond
-      ((null? _list) result)
-      ((element-of-set? (car _list) result) ;already part of result, do not need to add to result
-       (iter result (cdr _list)))
-      (else
-       (iter (append (list (car _list)) result) (cdr _list))
-       )
-      )
-    )
-  (iter '() (append set1 set2))
-  )
-
-;; (union-set set1 set2)
-;; (union-set (list 1 2 3) (list 2 3 4))
-;; (append set1 set2)
-
