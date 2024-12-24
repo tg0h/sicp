d41ede951e6372fc788c74478b3f47dbf509495d
tim goh <timslgoh@gmail.com>
2024-12-24 10:35:56 +0800

    02_building_abstractions_with_data/2.59.rkt

--- 02_building_abstractions_with_data/2.59.rkt
+++ 02_building_abstractions_with_data/2.59.rkt
@@ -5,13 +5,26 @@
         ((equal? x (car set)) true)
         (else (element-of-set? x (cdr set)))))
 
+(define set1 (list 1 2 3))
+(define set2 (list 4 5 6))
+(define set3 (list 3 4 5))
+
+(element-of-set? 1 set1 )
+(element-of-set? 5 set1 )
+
 (define (adjoin-set x set)
   (if (element-of-set? x set)
       set
       (cons x set)))
+
+(adjoin-set 9 set1)
+(adjoin-set 1 set1)
 
 (define (intersection-set set1 set2)
   (cond ((or (null? set1) (null? set2)) '())
         ((element-of-set? (car set1) set2)
          (cons (car set1) (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+(intersection-set set1 set2)
+(intersection-set set1 set3)
