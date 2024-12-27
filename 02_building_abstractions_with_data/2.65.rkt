e47eacc35c3aa10d2b4353ad6468c331dd93cbd0
tim goh <timslgoh@gmail.com>
2024-12-27 11:40:04 +0800

    02_building_abstractions_with_data/2.65.rkt

---
+++ 02_building_abstractions_with_data/2.65.rkt
@@ -0,0 +1,36 @@
+#lang sicp
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set) (cond ((null? set) false)
+                                      ((= x (entry set)) true)
+                                      ((< x (entry set))
+                                       (element-of-set? x (left-branch set)))
+                                      ((> x (entry set))
+                                       (element-of-set? x (right-branch set)))))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n) (if (= n 0)
+                                  (cons '() elts)
+                                  (let ((left-size (quotient (- n 1) 2)))
+                                    (let ((left-result
+                                           (partial-tree elts left-size)))
+                                      (let ((left-tree (car left-result))
+                                            (non-left-elts (cdr left-result))
+                                            (right-size (- n (+ left-size 1))))
+                                        (let ((this-entry (car non-left-elts)) (right-result
+                                                                                (partial-tree
+                                                                                 (cdr non-left-elts)
+                                                                                 right-size)))
+                                          (let ((right-tree (car right-result)) (remaining-elts
+                                                                                 (cdr right-result)))
+                                            (cons (make-tree this-entry
+                                                             left-tree
+                                                             right-tree)
+                                                  remaining-elts))))))))
