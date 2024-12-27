11e9f5384d96fc702da2a3767061a08cf7d90e29
tim goh <timslgoh@gmail.com>
2024-12-27 11:44:11 +0800

    02_building_abstractions_with_data/2.65.rkt

--- 02_building_abstractions_with_data/2.65.rkt
+++ 02_building_abstractions_with_data/2.65.rkt
@@ -22,30 +22,35 @@
                     (adjoin-set x (left-branch set))
                     (right-branch set)))
         ((> x (entry set))
-                  (make-tree (entry set) (left-branch set)
+         (make-tree (entry set) (left-branch set)
                     (adjoin-set x (right-branch set))))))
 
 
-         (define (list->tree elements)
-           (car (partial-tree elements (length elements))))
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
 
-         (define (partial-tree elts n) (if (= n 0)
-                                           (cons '() elts)
-                                           (let ((left-size (quotient (- n 1) 2)))
-                                             (let ((left-result
-                                                    (partial-tree elts left-size)))
-                                               (let ((left-tree (car left-result))
-                                                     (non-left-elts (cdr left-result))
-                                                     (right-size (- n (+ left-size 1))))
-                                                 (let ((this-entry (car non-left-elts)) (right-result
-                                                                                         (partial-tree
-                                                                                          (cdr non-left-elts)
-                                                                                          right-size)))
-                                                   (let ((right-tree (car right-result)) (remaining-elts
-                                                                                          (cdr right-result)))
-                                                     (cons (make-tree this-entry
-                                                                      left-tree
-                                                                      right-tree)
-                                                           remaining-elts))))))))
+(define (partial-tree elts n) (if (= n 0)
+                                  (cons '() elts)
+                                  (let ((left-size (quotient (- n 1) 2)))
+                                    (let ((left-result
+                                           (partial-tree elts left-size)))
+                                      (let ((left-tree (car left-result))
+                                            (non-left-elts (cdr left-result))
+                                            (right-size (- n (+ left-size 1))))
+                                        (let ((this-entry (car non-left-elts)) (right-result
+                                                                                (partial-tree
+                                                                                 (cdr non-left-elts)
+                                                                                 right-size)))
+                                          (let ((right-tree (car right-result)) (remaining-elts
+                                                                                 (cdr right-result)))
+                                            (cons (make-tree this-entry
+                                                             left-tree
+                                                             right-tree)
+                                                  remaining-elts))))))))
 
-         ;; (partial-tree (list 1 3 5 7 9 11) 6)
+;; (partial-tree (list 1 3 5 7 9 11) 6)
+
+(define set1 (list->tree (list 1 2 3)))
+
+(define set2 (list->tree (list 3 4 5)))
+
