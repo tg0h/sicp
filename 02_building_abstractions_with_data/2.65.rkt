ecfdb3140fcc3fe6317092dc6f353f5234512c8e
tim goh <timslgoh@gmail.com>
2024-12-27 11:51:00 +0800

    02_building_abstractions_with_data/2.65.rkt

--- 02_building_abstractions_with_data/2.65.rkt
+++ 02_building_abstractions_with_data/2.65.rkt
@@ -1,5 +1,16 @@
 #lang sicp
 
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
 (define (entry tree) (car tree))
 (define (left-branch tree) (cadr tree))
 (define (right-branch tree) (caddr tree))
