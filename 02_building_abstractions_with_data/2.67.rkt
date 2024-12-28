8e2a880a10222ab3449e992d74c7091876a23659
tim goh <timslgoh@gmail.com>
2024-12-28 10:07:22 +0800

    02_building_abstractions_with_data/2.67.rkt

--- 02_building_abstractions_with_data/2.67.rkt
+++ 02_building_abstractions_with_data/2.67.rkt
@@ -42,3 +42,11 @@
         ((< (weight x) (weight (car set))) (cons x set))
         (else (cons (car set)
                     (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair) ;symbol
+                               (cadr pair)) ; frequency
+                    (make-leaf-set (cdr pairs))))))
