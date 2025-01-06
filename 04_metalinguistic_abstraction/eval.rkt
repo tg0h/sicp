d75cb6b7eb2d00ed6b102cfccf92481ea9a32ee4
tim goh <timslgoh@gmail.com>
2025-01-06 11:41:12 +0800

    04_metalinguistic_abstraction/eval.rkt

--- 04_metalinguistic_abstraction/eval.rkt
+++ 04_metalinguistic_abstraction/eval.rkt
@@ -99,6 +99,7 @@
 (define (true? x) (not (eq? x false)))
 (define (false? x) (eq? x false))
 
+; represent procedures
 (define (make-procedure parameters body env) (list 'procedure parameters body env))
 (define (compound-procedure? p) (tagged-list? p 'procedure))
 (define (procedure-parameters p) (cadr p)) (define (procedure-body p) (caddr p))
