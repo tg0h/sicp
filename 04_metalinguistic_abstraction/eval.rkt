7445891994f5a3cb0e2366d89197dd33bd52704a
tim goh <timslgoh@gmail.com>
2025-01-06 11:24:53 +0800

    04_metalinguistic_abstraction/eval.rkt

--- 04_metalinguistic_abstraction/eval.rkt
+++ 04_metalinguistic_abstraction/eval.rkt
@@ -31,3 +31,20 @@
            (procedure-environment procedure))))
         (else (error
                "Unknown procedure type: APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env) 
+  (cond ((last-exp? exps)
+                                        (eval (first-exp exps) env))
+                                       (else
+                                        (eval (first-exp exps) env) (eval-sequence (rest-exps exps) env))))
