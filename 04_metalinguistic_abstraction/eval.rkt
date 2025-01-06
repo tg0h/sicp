5a53abd57a201769f8f28a2bd04afe1528f9d663
tim goh <timslgoh@gmail.com>
2025-01-06 11:27:16 +0800

    04_metalinguistic_abstraction/eval.rkt

--- 04_metalinguistic_abstraction/eval.rkt
+++ 04_metalinguistic_abstraction/eval.rkt
@@ -5,6 +5,10 @@
         ((string? exp) true) (else false)))
 (define (variable? exp) (symbol? exp))
 
+(define (assignment? exp) (tagged-list? exp 'set!))
+(define (assignment-variable exp) (cadr exp)) 
+(define (assignment-value exp) (caddr exp))
+
 (define (tagged-list? exp tag)
   (if (pair? exp)
       (eq? (car exp) tag)
