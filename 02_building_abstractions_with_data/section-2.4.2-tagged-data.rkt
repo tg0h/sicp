1fb32a480c576b034b17333db63a9ae9a1c465ba
tim goh <timslgoh@gmail.com>
2025-01-07 15:00:57 +0800

    02_building_abstractions_with_data/section-2.4.2-tagged-data.rkt

---
+++ 02_building_abstractions_with_data/section-2.4.2-tagged-data.rkt
@@ -0,0 +1,52 @@
+#lang sicp
+
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+(define (sub-complex z1 z2)
+  (make-from-real-imag (- (real-part z1) (real-part z2))
+                       (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+
+(define (square x) (* x x))car
+(define (real-part z) (car z))
+(define (imag-part z) (cdr z))
+(define (magnitude z)
+  (sqrt (+ (square (real-part z))
+           (square (imag-part z)))))
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+
+(define (make-from-real-imag x y) (cons x y))
+(define (make-from-mag-ang r a)
+  (cons (* r (cos a)) (* r (sin a))))
+
+; alyssa
+;; (define (real-part z) (* (magnitude z) (cos (angle z))))
+;; (define (imag-part z) (* (magnitude z) (sin (angle z))))
+;; (define (magnitude z) (car z))
+;; (define (angle z) (cdr z))
+;; (define (make-from-real-imag x y)
+;;   (cons (sqrt (+ (square x) (square y)))
+;;         (atan y x)))
+;; (define (make-from-mag-ang r a) (cons r a))
+
+
+(define (attach-tag type-tag contents) (cons type-tag contents))
+(define (type-tag datum) (if (pair? datum)
+                             (car datum)
+                             (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum) (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (rectangular? z) (eq? (type-tag z) 'rectangular))
+(define (polar? z) (eq? (type-tag z) 'polar))
