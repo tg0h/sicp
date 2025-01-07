72fef129e06b40f9c7fc79b01798b4dcfa28b99f
tim goh <timslgoh@gmail.com>
2025-01-07 15:04:34 +0800

    02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt

--- 02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt
+++ 02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt
@@ -56,6 +56,7 @@
     (cons (* r (cos a)) (* r (sin a))))
   ;; interface to the rest of the system
   (define (tag x) (attach-tag 'rectangular x))
+
   (put 'real-part '(rectangular) real-part)
   (put 'imag-part '(rectangular) imag-part)
   (put 'magnitude '(rectangular) magnitude)
@@ -81,3 +82,9 @@
   (put 'real-part '(polar) real-part)
   (put 'imag-part '(polar) imag-part)
   (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a)))) 
+  'done)
