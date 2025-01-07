22d655751cd3de73e52b4e4bb82ebf5f839c5f0a
tim goh <timslgoh@gmail.com>
2025-01-07 15:06:09 +0800

    02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt

--- 02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt
+++ 02_building_abstractions_with_data/section-2.4.3-data-directed-programming.rkt
@@ -86,5 +86,18 @@
   (put 'make-from-real-imag 'polar
        (lambda (x y) (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'polar
-       (lambda (r a) (tag (make-from-mag-ang r a)))) 
+       (lambda (r a) (tag (make-from-mag-ang r a))))
   'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z)) 
+(define (imag-part z) (apply-generic 'imag-part z)) 
+(define (magnitude z) (apply-generic 'magnitude z)) 
+(define (angle z) (apply-generic 'angle z))
