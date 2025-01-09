db70dca8b6bc3067c13118e9b39e69f1142eab3e
tim goh <timslgoh@gmail.com>
2025-01-09 10:39:19 +0800

    02_building_abstractions_with_data/section-2.5.1-generic-arithmetic-operations.rkt

--- 02_building_abstractions_with_data/section-2.5.1-generic-arithmetic-operations.rkt
+++ 02_building_abstractions_with_data/section-2.5.1-generic-arithmetic-operations.rkt
@@ -35,6 +35,25 @@
 (define put (operation-table 'insert-proc!))
 
 
+(define (attach-tag type-tag contents) (cons type-tag contents))
+(define (type-tag datum) (if (pair? datum)
+                             (car datum)
+                             (error "Bad tagged datum: TYPE-TAG" datum)))
+(define (contents datum)
+  (if (pair? datum) (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (apply-generic op . args)
+  ; use map type-tag because we want to be generic, we want to provide many contents and many types for the contents
+  ; for now only one arg eg real-part z not real-part z1 z2
+  (let ((type-tags (map type-tag args))) ; get the type-tag ('rectangular) from the args eg ('rectangular ( 1 . 1 ) )
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args)) ; get the contents ( ( 1 . 1 ) ) from the args eg ('rectangular ( 1 . 1 ) )
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
+
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
@@ -50,5 +69,5 @@
        (lambda (x y) (tag (* x y))))
   (put 'div '(scheme-number scheme-number)
        (lambda (x y) (tag (/ x y))))
-  (put 'make 'scheme-number (lambda (x) (tag x))) 
+  (put 'make 'scheme-number (lambda (x) (tag x)))
   'done)
