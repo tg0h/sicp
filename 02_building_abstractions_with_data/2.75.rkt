8d12845169e3567ed555854fc3dd3cf158c9dbec
tim goh <timslgoh@gmail.com>
2025-01-09 09:10:52 +0800

    02_building_abstractions_with_data/2.75.rkt

--- 02_building_abstractions_with_data/2.75.rkt
+++ 02_building_abstractions_with_data/2.75.rkt
@@ -11,7 +11,22 @@
           (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
   dispatch)
 
+
+(define (apply-generic op . args)
+  ; use map type-tag because we want to be generic, we want to provide many contents and many types for the contents
+  ; for now only one arg eg real-part z not real-part z1 z2
+  (let ((type-tags (map type-tag args))) ; get the type-tag ('rectangular) from the args eg ('rectangular ( 1 . 1 ) )
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args)) ; get the contents ( ( 1 . 1 ) ) from the args eg ('rectangular ( 1 . 1 ) )
+          (error "No method for these types: APPLY-GENERIC"
+                 (list op type-tags))))))
+
 (define (apply-generic op arg) (arg op))
 
 (define z1 (make-from-real-imag 1 1))
+z1
+(z1 'real-part)
+(z1 'imag-part)
+(z1 'magnitude)
 
