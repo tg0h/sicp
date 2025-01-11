27fb56474649b24530d546be05bbd92b3b6ef393
tim goh <timslgoh@gmail.com>
2025-01-11 14:41:02 +0800

    02_building_abstractions_with_data/2.84.rkt

--- 02_building_abstractions_with_data/2.84.rkt
+++ 02_building_abstractions_with_data/2.84.rkt
@@ -232,16 +232,18 @@
 ;; (define z3 (make-complex-from-real-imag 0 0))
 
 (define (scheme-number->complex n) (make-complex-from-real-imag (contents n) 0))
-(put-coercion 'scheme-number
-              'complex
-              scheme-number->complex)
 
 (define coerce-table (make-table))
 (define get-coercion (coerce-table 'lookup-proc))
 (define put-coercion (coerce-table 'insert-proc!))
 
+(put-coercion 'scheme-number
+              'complex
+              scheme-number->complex)
+
+(define tower '(integer rational real complex))
+
 (define (raise number)
-  (define tower '(integer rational real complex))
   (define (next-type type)
     (define (loop type type-list)
       (cond
@@ -277,5 +279,31 @@
                         (else (error "No method for these types" (list op type-tags))))))
               (error "No method for these types" (list op type-tags)))))))
 
+(define (_apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types" (list op type-tags))))))
+              (error "No method for these types" (list op type-tags)))))))
+
 
 ;; (get-coercion type raise-type)
+(define (type-lower? type1 type2)
+  ( if (eq? type1 type2) false
+       (memq type2 (memq type1 tower))
+       )
+  )
+
+(memq 'complex (memq 'complex tower))
