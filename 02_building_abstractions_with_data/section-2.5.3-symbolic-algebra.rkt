367120ace2d2771af4bad5b867b459a9f44657f6
tim goh <timslgoh@gmail.com>
2025-01-15 13:08:29 +0800

    02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt

--- 02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt
+++ 02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt
@@ -8,12 +8,14 @@
 (define (install-polynomial-package)
   ;; internal procedures
   ;; representation of poly
-  (define (make-poly variable term-list) (cons variable term-list)) (define (variable p) (car p))
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
   (define (term-list p) (cdr p))
   ;; ⟨procedures same-variable? and variable? from section 2.3.2⟩ ;; representation of terms and term lists
-(define (variable? x) (symbol? x))
-(define (same-variable? v1 v2)
-  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
   ;; ⟨procedures adjoin-term . . . coeff from text below⟩
   (define (add-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
