1cc482e3673da5d1872b55d619d263e8133d877c
tim goh <timslgoh@gmail.com>
2025-01-15 14:15:17 +0800

    03_modularity_objects_and_state/3.1.rkt

---
+++ 03_modularity_objects_and_state/3.1.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define (make-accumulator initial)
+  (define (add number)
+    (set! initial (+ initial number)
+          )
+    initial
+    )
+  add
+  )
+
+(define A (make-accumulator 5))
+
+(A 10)
+(A 10)
+
+
