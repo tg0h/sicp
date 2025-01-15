797d3c123bcc8e03018a10036ada31400e76ddb6
tim goh <timslgoh@gmail.com>
2025-01-15 14:24:53 +0800

    03_modularity_objects_and_state/3.2.rkt

--- 03_modularity_objects_and_state/3.2.rkt
+++ 03_modularity_objects_and_state/3.2.rkt
@@ -1,1 +1,26 @@
 #lang sicp
+
+(define (dummy x)
+  (display "I was called with ")
+  (display x)
+  (newline)
+  )
+
+(define (make-monitored f)
+  (let
+      ((counter 0))
+    (define (proxy x)
+      (cond
+        ((x eq? 'how-many-calls? ) counter)
+        ((x eq? 'reset-count ) set! counter 0)
+        (else
+         (begin
+           (set! counter (+ counter 1))
+           (f x)
+           )
+         )
+        )
+      )
+    proxy
+    )
+  )
