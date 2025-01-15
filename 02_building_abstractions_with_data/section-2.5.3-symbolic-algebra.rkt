37a813dd71a127ea947d1074a70cd9e74a8ed1ce
tim goh <timslgoh@gmail.com>
2025-01-15 12:57:30 +0800

    02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt

--- 02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt
+++ 02_building_abstractions_with_data/section-2.5.3-symbolic-algebra.rkt
@@ -1,5 +1,10 @@
 #lang sicp
 
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
 (define (add-poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
@@ -11,3 +16,22 @@
       (make-poly (variable p1)
                  (mul-terms (term-list p1) (term-list p2)))
       (error "Polys not in same var: MUL-POLY" (list p1 p2))))
+
+
+(define (install-polynomial-package)
+;; internal procedures
+;; representation of poly
+(define (make-poly variable term-list) (cons variable term-list)) (define (variable p) (car p))
+(define (term-list p) (cdr p))
+⟨procedures same-variable? and variable? from section 2.3.2⟩ ;; representation of terms and term lists
+⟨procedures adjoin-term . . . coeff from text below⟩
+(define (add-poly p1 p2) ...)
+⟨procedures used by add-poly⟩
+(define (mul-poly p1 p2) ...)
+⟨procedures used by mul-poly⟩
+;; interface to rest of the system
+(define (tag p) (attach-tag 'polynomial p))
+(put 'add '(polynomial polynomial)
+(lambda (p1 p2) (tag (add-poly p1 p2)))) (put 'mul '(polynomial polynomial)
+(lambda (p1 p2) (tag (mul-poly p1 p2)))) (put 'make 'polynomial
+(lambda (var terms) (tag (make-poly var terms)))) 'done)
