ae8fab724ac5326590f463c78977bd187a21039b
tim goh <timslgoh@gmail.com>
2025-01-15 16:31:39 +0800

    03_modularity_objects_and_state/section-3.1.2.rkt

--- 03_modularity_objects_and_state/section-3.1.2.rkt
+++ 03_modularity_objects_and_state/section-3.1.2.rkt
@@ -1,9 +1,15 @@
 #lang sicp
 
+(define (rand)
+  (random 10000)
+  )
+
 (define (estimate-pi trials)
   (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
 (define (cesaro-test)
   (= (gcd (rand) (rand)) 1))
+
 (define (monte-carlo trials experiment)
   (define (iter trials-remaining trials-passed)
     (cond ((= trials-remaining 0)
@@ -15,3 +21,13 @@
            (iter (- trials-remaining 1)
                  trials-passed))))
   (iter trials 0))
+
+
+
+
+(monte-carlo 10000 cesaro-test)
+
+;; (cesaro-test)
+
+;; (gcd (rand) (rand))
+;; (gcd 1 2)
