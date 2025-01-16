de101d10bef284a3d716c8c1969adc6c754f52b1
tim goh <timslgoh@gmail.com>
2025-01-16 10:41:32 +0800

    03_modularity_objects_and_state/3.7.rkt

--- 03_modularity_objects_and_state/3.7.rkt
+++ 03_modularity_objects_and_state/3.7.rkt
@@ -8,14 +8,26 @@
   (define (deposit amount)
     (set! balance (+ balance amount))
     balance)
+  (define password-list nil)
+  (define (add-password password)
+    (set! password-list (cons password password-list))
+    )
+  (define (password-correct? password)
+    (memq password password-list)
+    )
   (define (dispatch pw m)
     (cond
-      ((not (eq? pw password)) (error "Incorrect Password"))
-      ((and (eq? pw password) (eq? m 'withdraw)) withdraw)
-      ((and (eq? pw password) (eq? m 'deposit)) deposit)
+      ((not (password-correct? password)) (error "Incorrect Password"))
+      ((and (password-correct? password) (eq? m 'withdraw)) withdraw)
+      ((and (password-correct? password) (eq? m 'deposit)) deposit)
       (else (error "Unknown request: MAKE-ACCOUNT"
                    m))))
-  dispatch)
+  (add-password password)
+  dispatch
+  )
+
+(define (make-joint account password new-password)
+  )
 
 
 (define acc (make-account 100 'secret-password))
@@ -25,3 +37,6 @@
 ((acc 'secret-password 'withdraw) 40)
 
 ((acc 'some-other-password 'deposit) 50) "Incorrect password"
+
+(define paul-acc
+  (make-joint peter-acc 'open-sesame 'rosebud))
