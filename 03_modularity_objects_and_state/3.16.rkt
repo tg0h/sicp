03af2c05c320a2e28b19d33d1bc397374ab340e8
tim goh <timslgoh@gmail.com>
2025-01-17 13:18:09 +0800

    03_modularity_objects_and_state/3.16.rkt

--- 03_modularity_objects_and_state/3.16.rkt
+++ 03_modularity_objects_and_state/3.16.rkt
@@ -1,8 +1,16 @@
 #lang sicp
 
-(define (count-pairs x) 
+(define (count-pairs x)
   (if (not (pair? x))
-                            0
-                            (+ (count-pairs (car x))
-                               (count-pairs (cdr x))
-                               1)))
+      0
+      (+ (count-pairs (car x))
+         (count-pairs (cdr x))
+         1)))
+
+(define l3 '( a b c))
+
+l3
+
+(count-pairs l3)
+
+(count-pairs (cons (list 'a) (list 'b)))
