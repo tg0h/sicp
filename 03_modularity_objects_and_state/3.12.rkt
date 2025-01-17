949bab30312496bbfb28db2cc118d74c2fdc5bc5
tim goh <timslgoh@gmail.com>
2025-01-17 11:29:34 +0800

    03_modularity_objects_and_state/3.12.rkt

--- 03_modularity_objects_and_state/3.12.rkt
+++ 03_modularity_objects_and_state/3.12.rkt
@@ -5,7 +5,6 @@
       y
       (cons (car x) (append (cdr x) y))))
 
-
 (define (append! x y)
   (set-cdr! (last-pair x) y) x)
 
@@ -13,3 +12,16 @@
   (if (null? (cdr x))
       x
       (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+
+z
+
+(cdr x)
+
+(define w (append! x y))
+w
+
+(cdr x)
