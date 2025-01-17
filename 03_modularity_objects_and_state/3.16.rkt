b6105f2ec64485c3aa11e80df4fa78a45be1244c
tim goh <timslgoh@gmail.com>
2025-01-17 13:55:42 +0800

    03_modularity_objects_and_state/3.16.rkt

--- 03_modularity_objects_and_state/3.16.rkt
+++ 03_modularity_objects_and_state/3.16.rkt
@@ -9,14 +9,23 @@
 
 (define l3 '( a b c))
 
-l3
+;; l3
 
-(count-pairs l3) ; 3
-
-(count-pairs (cons (list 'a) (list 'b))) ; also 3
+;; (count-pairs l3) ; 3
+;; (count-pairs (cons (list 'a) (list 'b))) ; also 3
 
 (define ab '(a b))
-ab
 
-(count-pairs (cons ab ab))
+(define z (cons ab (cdr ab)))
+;; (set-cdr! z (cdr ab))
+
+(car z)
+(cdr z)
+(count-pairs z)
+
+;; (count-pairs (cons ab ab)) ; 5
+
+(define my-structure (cons (cons 1 2) (cons (cons 3 4) (cons 5 6))))
+
+(count-pairs my-structure)
 
