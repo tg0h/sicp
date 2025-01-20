48e9f92771b7fa68d5d5b32dc699b9b01e949eb1
tim goh <timslgoh@gmail.com>
2025-01-20 12:44:27 +0800

    03_modularity_objects_and_state/3.17.rkt

--- 03_modularity_objects_and_state/3.17.rkt
+++ 03_modularity_objects_and_state/3.17.rkt
@@ -4,54 +4,27 @@
 
 (define (count-pairs x)
   (define (counted-before? x search-list)
-    ;; (display "search: ")
-    ;; (display x)
-    ;; (display " in: ")
-    ;; (display search-list)
     (cond ((null? search-list)
-           ;; (display " FALSE")
-           ;; (newline)
            false
            )
           ((eq? x (car search-list))
-           ;; (display " true")
-           ;; (newline)
            true
            )
           (else
-           ;; (display " ->> ")
            (counted-before? x (cdr search-list))
            ))
     )
   (define (count x)
-    ;; (display "count >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> x is:")
-    ;; (display x)
-    ;; (display " | counted is:")
-    ;; (display counted)
-    ;; (newline)
     (cond
       ((not (pair? x))
-       ;; (display "return 0")
-       ;; (newline)
        0
        )
       ((counted-before? x counted)
-       ;; (display "COUNTED BEFORE")
        (+ (count (car x))
           (count (cdr x)))
        )
       (else
-       ;; (display "add x:")
-       ;; (display x)
-       ;; (newline)
-       ;; (display " counted: ")
-       ;; (display counted)
-       ;; (newline)
        (set! counted (cons x counted))
-       ;; (display "after add: ")
-       ;; (display counted)
-       ;; (newline)
-       ;; (newline)
        (+ (count (car x))
           (count (cdr x))
           1 )
@@ -63,21 +36,9 @@
   )
 
 (define l3 '( a b c))
-;; (count-pairs l3)
-
-
-;; (define ab '(a b))
-;; (define x ab)
-;; (set-cdr! ab 'c)
-;; x
-;; ab
-
-;; (eq? x ab)
 (define a '(a ))
 
-;; (define abab (cons ab ab))
 (define aa (cons a a))
-;; (count-pairs abab)
 
 (count-pairs aa)
 
@@ -86,13 +47,3 @@
 (define _a (cons a a))
 (count-pairs (cons _a _a))
 
-;; (eq? _a _a)
-
-;; (define (search x counted)
-;;   (cond ((null? counted) false)
-;;         ((eq? x (car counted)) true)
-;;         (else (search x (cdr counted))))
-;;   )
-;; (search l3 counted)
-
-;; (eq? ab ab)
