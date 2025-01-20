1e830890687e91320dcc444b3393dec9b3cd8308
tim goh <timslgoh@gmail.com>
2025-01-20 15:07:49 +0800

    03_modularity_objects_and_state/section-3.3.2-representing-queues.rkt

---
+++ 03_modularity_objects_and_state/section-3.3.2-representing-queues.rkt
@@ -0,0 +1,8 @@
+#lang sicp
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
