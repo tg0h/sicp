570a2b4b9a6a64cc84ecf418c4255c74b0967a1a
tim goh <timslgoh@gmail.com>
2025-01-21 12:15:29 +0800

    03_modularity_objects_and_state/section-3.3.3-2d-tables.rkt

--- 03_modularity_objects_and_state/section-3.3.3-2d-tables.rkt
+++ 03_modularity_objects_and_state/section-3.3.3-2d-tables.rkt
@@ -30,12 +30,13 @@
 (define (make-table)
   (let ((local-table (list '*table*)))
     (define (lookup key-1 key-2)
-      (let ((subtable
-             (assoc key-1 (cdr local-table))))
+      (let ((subtable (assoc key-1 (cdr local-table))))
         (if subtable
-            (let ((record
-                   (assoc key-2 (cdr subtable))))
-              (if record (cdr record) false)) false)))
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
     (define (insert! key-1 key-2 value)
       (let ((subtable
              (assoc key-1 (cdr local-table))))
