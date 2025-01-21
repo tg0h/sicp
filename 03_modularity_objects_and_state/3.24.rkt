10612745422ba1eebba67a2350266d1eadd67044
tim goh <timslgoh@gmail.com>
2025-01-21 12:48:33 +0800

    03_modularity_objects_and_state/3.24.rkt

--- 03_modularity_objects_and_state/3.24.rkt
+++ 03_modularity_objects_and_state/3.24.rkt
@@ -1,24 +1,32 @@
 #lang sicp
 
-(define (lookup key table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (cdr record)
-        false)))
 
-(define (assoc key records)
-  (cond ((null? records) false)
-        ((equal? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
-
-(define (insert! key value table)
-  (let ((record (assoc key (cdr table))))
-    (if record
-        (set-cdr! record value)
-        (set-cdr! table
-                  (cons (cons key value)
-                        (cdr table)))))
-  'ok)
+;; (define (make-table)
+;; (list '*table*))
 
 (define (make-table)
-  (list '*table*))
+  (let ((local-table (list '*table*)))
+    (define (lookup key table)
+      (let ((record (assoc key (cdr table))))
+        (if record
+            (cdr record)
+            false)))
+
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((equal? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (insert! key value table)
+      (let ((record (assoc key (cdr table))))
+        (if record
+            (set-cdr! record value)
+            (set-cdr! table
+                      (cons (cons key value)
+                            (cdr table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
