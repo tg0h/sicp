8198968794468bac288bced8c8c359fd7ab98469
tim goh <timslgoh@gmail.com>
2025-01-21 13:03:38 +0800

    03_modularity_objects_and_state/3.24.rkt

--- 03_modularity_objects_and_state/3.24.rkt
+++ 03_modularity_objects_and_state/3.24.rkt
@@ -6,22 +6,25 @@
 
 (define (make-table same-key?)
   (let ((local-table (list '*table*)))
-    (define (lookup key table)
-      (let ((record (assoc key (cdr table))))
+
+    (define (lookup key)
+      (let ((record (assoc key (cdr local-table))))
         (if record
             (cdr record)
             false)))
+
     (define (assoc key records)
       (cond ((null? records) false)
             ((same-key? key (caar records)) (car records))
             (else (assoc key (cdr records)))))
-    (define (insert! key value table)
-      (let ((record (assoc key (cdr table))))
+
+    (define (insert! key value )
+      (let ((record (assoc key (cdr local-table))))
         (if record
             (set-cdr! record value)
-            (set-cdr! table
+            (set-cdr! local-table
                       (cons (cons key value)
-                            (cdr table)))))
+                            (cdr local-table)))))
       'ok)
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
@@ -44,7 +47,10 @@
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
 
-((operation-table 'insert-proc!) 10 'tim)
+;; ((operation-table 'insert-proc!) 10 'tim)
 
-;; (put 10 'tim)
-;; (get 10 'tim)
+(put 10 'tim)
+(get 10)
+(get 9)
+(get 5)
+(get 4)
