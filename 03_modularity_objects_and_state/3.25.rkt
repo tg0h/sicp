d4f10ef3790f0c15e66d0d0d16a971a9bcf139b3
tim goh <timslgoh@gmail.com>
2025-01-22 11:40:26 +0800

    03_modularity_objects_and_state/3.25.rkt

--- 03_modularity_objects_and_state/3.25.rkt
+++ 03_modularity_objects_and_state/3.25.rkt
@@ -11,20 +11,25 @@
               false))
         false)))
 
-(define (insert! key-1 key-2 value table)
-  (let ((subtable (assoc key-1 (cdr table))))
-    (if subtable
-        (let ((record (assoc key-2 (cdr subtable))))
-          (if record
-              (set-cdr! record value)
-              (set-cdr! subtable
-                        (cons (cons key-2 value)
-                              (cdr subtable)))))
-        (set-cdr! table
-                  (cons (list key-1
-                              (cons key-2 value))
-                        (cdr table)))))
-  'ok)
+;; (define (insert! key-1 key-2 value table)
+;;   (let ((subtable (assoc key-1 (cdr table))))
+;;     (if subtable
+;;         (let ((record (assoc key-2 (cdr subtable))))
+;;           (if record
+;;               (set-cdr! record value)
+;;               (set-cdr! subtable
+;;                         (cons (cons key-2 value)
+;;                               (cdr subtable)))))
+;;         (set-cdr! table
+;;                   (cons (list key-1
+;;                               (cons key-2 value))
+;;                         (cdr table)))))
+;;   'ok)
+
+(define (insert! keys value table)
+  (if (null? keys) error "keys are empty")
+
+  )
 
 (define (make-table)
   (let ((local-table (list '*table*)))
