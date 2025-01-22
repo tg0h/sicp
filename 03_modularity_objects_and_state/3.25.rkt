cc5e6e5adbbb3d7be73d7980a8e9eab99ba68f17
tim goh <timslgoh@gmail.com>
2025-01-22 12:08:59 +0800

    03_modularity_objects_and_state/3.25.rkt

--- 03_modularity_objects_and_state/3.25.rkt
+++ 03_modularity_objects_and_state/3.25.rkt
@@ -1,5 +1,33 @@
 #lang sicp
 
+;; (define (lookup keys table)
+;;   (let ((key (car keys))
+;;         (is-last-key? (null? (cdr keys))))
+;;     (let ((record (assoc key (cdr table))))
+;;       (if record
+;;           (if is-last-key?
+;;               record
+;;               (lookup (cdr keys) record))
+;;           false
+;;           ))))
+;; (define (insert! keys value table)
+;;   (define (create-table keys value)
+;;     (cons (car keys)
+;;           (if (null? (cdr keys))
+;;               value
+;;               (list (create-table (cdr keys) value)))))
+;;   (if (null? keys) error "keys are empty")
+;;   (let ((key (car keys))
+;;         (is-last-key? (null? (cdr keys))))
+;;     (let ((record (assoc key (cdr table))))
+;;       (if record
+;;           (if (is-last-key?)
+;;               (set-cdr! record value)
+;;               (insert! (cdr keys) value record))
+;;           (set-cdr! table (cons
+;;                            (create-table keys value)
+;;                            value)))))
+;;   'ok)
 
 (define (make-table)
   (let ((local-table (list '*table*)))
@@ -41,5 +69,4 @@
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
 
-(put 'math '+ 43)
-(get 'math '+)
+(put '(1 2) 3)
