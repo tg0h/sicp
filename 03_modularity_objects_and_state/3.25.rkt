9d28970216a6f9577d46930adc972cd6b8bf5e77
tim goh <timslgoh@gmail.com>
2025-01-22 12:07:40 +0800

    03_modularity_objects_and_state/3.25.rkt

--- 03_modularity_objects_and_state/3.25.rkt
+++ 03_modularity_objects_and_state/3.25.rkt
@@ -1,5 +1,8 @@
 #lang sicp
 
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
 (define (lookup keys table)
   (let ((key (car keys))
         (is-last-key? (null? (cdr keys))))
@@ -29,33 +32,6 @@
                            (create-table keys value)
                            value)))))
   'ok)
-
-
-(define (make-table)
-  (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
-            false)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable
-             (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record
-                   (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1 (cons key-2 value))
-                            (cdr local-table)))))
-      'ok)
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
             ((eq? m 'insert-proc!) insert!)
