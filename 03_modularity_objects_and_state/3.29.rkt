4e69543e69cd183f95a7a8c53b0921b548fd6bbe
tim goh <timslgoh@gmail.com>
2025-01-24 12:30:43 +0800

    03_modularity_objects_and_state/3.29.rkt

--- 03_modularity_objects_and_state/3.29.rkt
+++ 03_modularity_objects_and_state/3.29.rkt
@@ -23,16 +23,16 @@
 ;;         ((and (= a 0) (= b 0) ) 0)
 ;;         (else (error "Invalid signal"))))
 
-(define (or-gate a1 a2 output)
-  (define (or-action-procedure)
-    (let ((new-value
-           (logical-or (get-signal a1) (get-signal a2))))
-      (after-delay
-       or-gate-delay
-       (lambda () (set-signal! output new-value)))))
-  (add-action! a1 or-action-procedure)
-  (add-action! a2 or-action-procedure)
-  )
+;; (define (or-gate a1 a2 output)
+;;   (define (or-action-procedure)
+;;     (let ((new-value
+;;            (logical-or (get-signal a1) (get-signal a2))))
+;;       (after-delay
+;;        or-gate-delay
+;;        (lambda () (set-signal! output new-value)))))
+;;   (add-action! a1 or-action-procedure)
+;;   (add-action! a2 or-action-procedure)
+;;   )
 
 (define (inverter input output)
   (define (invert-input)
@@ -44,3 +44,33 @@
 (define (logical-not s)
   (cond ((= s 0) 1) ((= s 1) 0)
         (else (error "Invalid signal" s))))
+
+
+
+;; (define (half-adder a b s c)
+;; (let ((d (make-wire)) (e (make-wire)))
+;;     (or-gate a b d)
+;;     (and-gate a b c)
+;;     (inverter c e)
+;;     (and-gate d e s)
+;;     'ok))
+
+(define (or-gate a b output)
+  (let (
+        (c (make-wire))
+        (d (make-wire))
+        (e (make-wire))
+        (f (make-wire))
+        (g (make-wire))
+        (h (make-wire))
+        (i (make-wire))
+        )
+    (and-gate a b c)
+    (inverter c d)
+    (inverter a e)
+    (and-gate d e f)
+    (inverter b h)
+    (and-gate e h i)
+    (and-gate f i g)
+    (inverter g output)
+    'ok))
