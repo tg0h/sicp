19f8dd58162d2cb5a1cdacd2cb573cde39c98048
tim goh <timslgoh@gmail.com>
2025-01-24 11:29:56 +0800

    03_modularity_objects_and_state/3.28.rkt

--- 03_modularity_objects_and_state/3.28.rkt
+++ 03_modularity_objects_and_state/3.28.rkt
@@ -1,21 +1,21 @@
 #lang sicp
 
-(define (and-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let ((new-value
-           (logical-and (get-signal a1) (get-signal a2))))
-      (after-delay
-       and-gate-delay
-       (lambda () (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
-  'ok
-  )
+;; (define (and-gate a1 a2 output)
+;;   (define (and-action-procedure)
+;;     (let ((new-value
+;;            (logical-and (get-signal a1) (get-signal a2))))
+;;       (after-delay
+;;        and-gate-delay
+;;        (lambda () (set-signal! output new-value)))))
+;;   (add-action! a1 and-action-procedure)
+;;   (add-action! a2 and-action-procedure)
+;;   'ok
+;;   )
 
-(define (logical-not s)
-  (cond ((= s 0) 1)
-        ((= s 1) 0)
-        (else (error "Invalid signal" s))))
+;; (define (logical-not s)
+;;   (cond ((= s 0) 1)
+;;         ((= s 1) 0)
+;;         (else (error "Invalid signal" s))))
 
 (define (logical-or a b)
   (cond ((= a 1) 1)
