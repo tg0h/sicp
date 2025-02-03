b41a3cf5d2450671b13f96a6e9c65505566ea4ea
tim goh <timslgoh@gmail.com>
2025-02-03 11:09:53 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -102,7 +102,15 @@
 (define and-gate-delay 3)
 (define or-gate-delay 5)
 
-(define input-1 (make-wire)) 
-(define input-2 (make-wire)) 
-(define sum (make-wire)) 
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum (make-wire))
 (define carry (make-wire))
+
+(probe 'sum sum)
+(probe 'carry carry)
+(half-adder input-1 input-2 sum carry)
+
+(set-signal! input-1 1)
+
+(propagate)
