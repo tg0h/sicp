c8dbc7a81326d96c630ad7ee2c2a15a577ff0b5f
tim goh <timslgoh@gmail.com>
2025-02-03 10:45:24 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -29,3 +29,17 @@
   ((wire 'set-signal!) new-value))
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
+
+(define (after-delay delay action)
+  (add-to-agenda!
+   (+ delay (current-time the-agenda))
+   action
+   the-agenda))
+
+(define (propagate)
+(if (empty-agenda? the-agenda)
+'done
+(let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
