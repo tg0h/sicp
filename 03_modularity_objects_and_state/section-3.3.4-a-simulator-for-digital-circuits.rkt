23455d1398a6179f0f6c1c6c5713d2104af819a0
tim goh <timslgoh@gmail.com>
2025-02-03 14:54:23 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -102,9 +102,45 @@
                (lambda ()
                  (newline)
                  (display name) (display " ")
-                 (display (current-time the-agenda)) 
+                 (display (current-time the-agenda))
                  (display " New-value = ")
                  (display (get-signal wire)))))
+
+
+(define (make-time-segment time queue)
+  (cons time queue))
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item)
+  (set-car! queue item))
+(define (set-rear-ptr! queue item)
+  (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (cdr (front-ptr queue)))
+              queue)))
 
 (define the-agenda (make-agenda))
 (define inverter-delay 2)
