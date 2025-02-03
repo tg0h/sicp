495d76e1142faca47250cf0468e8601901b88e87
tim goh <timslgoh@gmail.com>
2025-02-03 10:44:44 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -23,3 +23,9 @@
   (if (null? procedures) 'done
       (begin ((car procedures))
              (call-each (cdr procedures)))))
+
+(define (get-signal wire) (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
