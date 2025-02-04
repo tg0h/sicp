6da452303798c9c1355772f728be102a7f2c5a29
tim goh <timslgoh@gmail.com>
2025-02-04 08:40:33 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -148,6 +148,12 @@
          (cons (make-new-time-segment time action)
                segments))
         (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q) 
+    (if (empty-queue? q)
+                          (set-segments! agenda (rest-segments agenda)))))
 ; -----------------------------------------------------------
 
 ; queue -----------------------------------------------------
