c9200c766237fbd806bbe95c904498cbed1dcaa0
tim goh <timslgoh@gmail.com>
2025-02-04 11:58:57 +0800

    03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt

--- 03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt
+++ 03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt
@@ -5,7 +5,8 @@
 (define F (make-connector))
 (celsius-fahrenheit-converter C F)
 
-(define (celsius-fahrenheit-converter c f) (let ((u (make-connector))
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
         (v (make-connector))
         (w (make-connector))
         (x (make-connector))
@@ -18,7 +19,38 @@
     (constant 32 y)
     'ok))
 
+(define (adder a1 a2 sum)
+  (define (process-new-value)
+    (cond ((and (has-value? a1)
+                (has-value? a2))
+           (set-value! sum
+                       (+ (get-value a1) (get-value a2))
+                       me))
+          ((and (has-value? a1) (has-value? sum))
+           (set-value! a2
+                       (- (get-value sum) (get-value a1))
+                       me))
+          ((and (has-value? a2) (has-value? sum))
+           (set-value! a1
+                       (- (get-value sum) (get-value a2))
+                       me))))
+  (define (process-forget-value)
+    (forget-value! sum me)
+    (forget-value! a1 me)
+    (forget-value! a2 me)
+    (process-new-value))
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value) (process-new-value))
+          ((eq? request 'I-lost-my-value) (process-forget-value))
+          (else (error "Unknown request: ADDER" request)))) (connect a1 me)
+  (connect a2 me)
+  (connect sum me)
+  me)
+
 (probe "Celsius temp" C)
 (probe "Fahrenheit temp" F)
 
 (set-value! C 25 'user)
+(set-value! F 212 'user)
+(forget-value! C 'user)
+(set-value! F 212 'user)
