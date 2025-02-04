5b1c33dc95a90f74730873742dcbb99f89e25dc9
tim goh <timslgoh@gmail.com>
2025-02-04 12:07:37 +0800

    03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt

--- 03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt
+++ 03_modularity_objects_and_state/section-3.3.5-propagation-of-constraints.rkt
@@ -1,24 +1,5 @@
 #lang sicp
 
-
-(define C (make-connector))
-(define F (make-connector))
-(celsius-fahrenheit-converter C F)
-
-(define (celsius-fahrenheit-converter c f)
-  (let ((u (make-connector))
-        (v (make-connector))
-        (w (make-connector))
-        (x (make-connector))
-        (y (make-connector)))
-    (multiplier c w u)
-    (multiplier v x u)
-    (adder v y f)
-    (constant 9 w)
-    (constant 5 x)
-    (constant 32 y)
-    'ok))
-
 (define (adder a1 a2 sum)
   (define (process-new-value)
     (cond ((and (has-value? a1)
@@ -160,6 +141,24 @@
 (define (set-value! connector new-value informant) ((connector 'set-value!) new-value informant))
 (define (forget-value! connector retractor) ((connector 'forget) retractor))
 (define (connect connector new-constraint) ((connector 'connect) new-constraint))
+
+(define C (make-connector))
+(define F (make-connector))
+(celsius-fahrenheit-converter C F)
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+        (v (make-connector))
+        (w (make-connector))
+        (x (make-connector))
+        (y (make-connector)))
+    (multiplier c w u)
+    (multiplier v x u)
+    (adder v y f)
+    (constant 9 w)
+    (constant 5 x)
+    (constant 32 y)
+    'ok))
 ;--------------------------
 
 (probe "Celsius temp" C)
