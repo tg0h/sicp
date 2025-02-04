70e388a26643c72865f49aa807ab2413aaf7925e
tim goh <timslgoh@gmail.com>
2025-02-04 08:40:03 +0800

    03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt

--- 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
+++ 03_modularity_objects_and_state/section-3.3.4-a-simulator-for-digital-circuits.rkt
@@ -107,21 +107,19 @@
                  (display (get-signal wire)))))
 
 ; agenda ----------------------------------------------------
+;; time segment
 (define (make-time-segment time queue)
   (cons time queue))
 (define (segment-time s) (car s))
 (define (segment-queue s) (cdr s))
-
+;; agenda
 (define (make-agenda) (list 0))
 (define (current-time agenda) (car agenda))
-(define (set-current-time! agenda time)
-  (set-car! agenda time))
+(define (set-current-time! agenda time) (set-car! agenda time))
 (define (segments agenda) (cdr agenda))
-(define (set-segments! agenda segments)
-  (set-cdr! agenda segments))
+(define (set-segments! agenda segments) (set-cdr! agenda segments))
 (define (first-segment agenda) (car (segments agenda)))
 (define (rest-segments agenda) (cdr (segments agenda)))
-
 (define (empty-agenda? agenda) (null? (segments agenda)))
 
 (define (add-to-agenda! time action agenda)
