7a7eb0cb6f4bccc94927f6381f39a05472ef0195
tim goh <timslgoh@gmail.com>
2025-02-06 13:20:22 +0800

    03_modularity_objects_and_state/3.45.rkt

--- 03_modularity_objects_and_state/3.45.rkt
+++ 03_modularity_objects_and_state/3.45.rkt
@@ -62,6 +62,12 @@
         (d (account 'deposit)))
     ((s d) amount)))
 
+(define (exchange account1 account2)
+(let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
 (define (serialized-exchange account1 account2)
   (let ((serializer1 (account1 'serializer))
         (serializer2 (account2 'serializer)))
