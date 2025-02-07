ebb83c05899c38a91af54cf6d0369df7f81cb029
tim goh <timslgoh@gmail.com>
2025-02-07 09:28:47 +0800

    03_modularity_objects_and_state/3.48.rkt

--- 03_modularity_objects_and_state/3.48.rkt
+++ 03_modularity_objects_and_state/3.48.rkt
@@ -33,3 +33,16 @@
             ((eq? m 'serializer) balance-serializer)
             (else (error "Unknown request: MAKE-ACCOUNT" m))))
     dispatch))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange)) ; lock both accounts
+     account1
+     account2)))
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference) ; this locks itself
+    ((account2 'deposit) difference)))
