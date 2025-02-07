8a0efe4c507f8f05d21c86547c9c661f53b5cb30
tim goh <timslgoh@gmail.com>
2025-02-07 17:36:06 +0800

    03_modularity_objects_and_state/3.51.rkt

--- 03_modularity_objects_and_state/3.51.rkt
+++ 03_modularity_objects_and_state/3.51.rkt
@@ -39,8 +39,13 @@
                        (stream-cdr stream))))
         (else (stream-filter pred (stream-cdr stream)))))
 
+
+(define (show x) (display-line x) x)
+
+;-----------------------
 (define x
   (stream-map show
               (stream-enumerate-interval 0 10)))
+; show 0
 (stream-ref x 5)
 (stream-ref x 7)
