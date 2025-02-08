493ca3d907f2324f803504ee91b5972f235eae02
tim goh <timslgoh@gmail.com>
2025-02-08 14:01:16 +0800

    03_modularity_objects_and_state/section-3.5.2-infinite-streams.rkt

--- 03_modularity_objects_and_state/section-3.5.2-infinite-streams.rkt
+++ 03_modularity_objects_and_state/section-3.5.2-infinite-streams.rkt
@@ -44,7 +44,7 @@
 
 (define (divisible? x y) (= (remainder x y) 0))
 (define (integers-starting-from n)
-(cons-stream n (integers-starting-from (+ n 1))))
+  (cons-stream n (integers-starting-from (+ n 1))))
 (define integers (integers-starting-from 1))
 
 (define (sieve stream) (cons-stream
@@ -54,3 +54,6 @@
                                   (not (divisible? x (stream-car stream))))
                                 (stream-cdr stream)))))
 (define primes (sieve (integers-starting-from 2)))
+
+(stream-ref primes 6)
+
