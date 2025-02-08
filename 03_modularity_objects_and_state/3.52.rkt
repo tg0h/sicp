f62024a9a92dab3d692ac0a84e64446f4dbcf6a2
tim goh <timslgoh@gmail.com>
2025-02-08 13:20:47 +0800

    03_modularity_objects_and_state/3.52.rkt

--- 03_modularity_objects_and_state/3.52.rkt
+++ 03_modularity_objects_and_state/3.52.rkt
@@ -24,6 +24,24 @@
                        (stream-cdr stream))))
         (else (stream-filter pred (stream-cdr stream)))))
 
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+(define (display-stream s) (stream-for-each display-line s))
+(define (display-line x) (newline)
+  (display "SHOW x-> ")
+  (display x)
+  (newline)
+  )
+;-------- basic ref funcs
+
 (define sum 0)
 (define (accum x) (set! sum (+ x sum)) sum)
 (define seq
