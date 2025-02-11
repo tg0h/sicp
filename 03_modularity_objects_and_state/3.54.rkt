9d45d494dc85ad1a92e730719ac8fc4aaf00fc7e
tim goh <timslgoh@gmail.com>
2025-02-11 09:00:55 +0800

    03_modularity_objects_and_state/3.54.rkt

--- 03_modularity_objects_and_state/3.54.rkt
+++ 03_modularity_objects_and_state/3.54.rkt
@@ -1,10 +1,13 @@
 #lang sicp
 
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
 (define (mul-streams s1 s2)
   (if (stream-null? s1) the-empty-stream
       (cons-stream
        (* (stream-car s1) (stream-car s2))
-       (mul-streams (steam-cdr s1) (stream-cdr s2))
+       (mul-streams (stream-cdr s1) (stream-cdr s2))
        )
       )
   )
