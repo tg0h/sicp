31fd3d18c2b2631d323ffd2007c2a3acad3d3718
tim goh <timslgoh@gmail.com>
2025-02-12 11:38:05 +0800

    03_modularity_objects_and_state/3.62.rkt

--- 03_modularity_objects_and_state/3.62.rkt
+++ 03_modularity_objects_and_state/3.62.rkt
@@ -79,3 +79,13 @@
                 -1)
                )
   )
+
+
+(define (div-series s1 s2)
+  (let ((c (stream-car s2)))
+    (if (= c 0)
+        (error "constant term of s2 can't be 0!")
+        (scale-stream
+         (mul-series s1 (invert-unit-series
+                         (scale-stream s2 (/ 1 c))))
+         (/ 1 c)))))
