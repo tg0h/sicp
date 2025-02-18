b55a844305f5c11e110930e3048871ba04b3484d
tim goh <timslgoh@gmail.com>
2025-02-19 07:03:38 +0800

    03_modularity_objects_and_state/3.64.rkt

--- 03_modularity_objects_and_state/3.64.rkt
+++ 03_modularity_objects_and_state/3.64.rkt
@@ -18,5 +18,25 @@
     (if (< (abs (- next current))  tolerance)
         next
         (stream-limit (stream-cdr s))))
-
   )
+
+
+(define
+  (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+
+(define (average x y) (/ (+ x y) 2))
+(define (sqrt-improve guess x) (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream
+     1.0
+     (stream-map (lambda (guess) (sqrt-improve guess x)) guesses)))
+  guesses)
