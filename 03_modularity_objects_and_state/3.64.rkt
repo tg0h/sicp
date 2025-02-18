8483aafb980eea4ea7f3e26b720b764f859b1682
tim goh <timslgoh@gmail.com>
2025-02-19 07:09:30 +0800

    03_modularity_objects_and_state/3.64.rkt

--- 03_modularity_objects_and_state/3.64.rkt
+++ 03_modularity_objects_and_state/3.64.rkt
@@ -12,13 +12,20 @@
 (define (stream-cdr stream) (force (cdr stream)))
 
 (define (stream-limit s tolerance)
-  (let (
-        (next (stream-car (stream-cdr s)))
+  (let ((next (stream-car (stream-cdr s)))
         (current (stream-car s)))
+    (display "next: ")
+    (display next)
+    (newline)
+    (display "current: ")
+    (display current)
+    (newline)
+    (display (abs (- next current)))
+    (newline)
+    (newline)
     (if (< (abs (- next current))  tolerance)
         next
-        (stream-limit (stream-cdr s))))
-  )
+        (stream-limit (stream-cdr s) tolerance))))
 
 
 (define
@@ -40,3 +47,19 @@
      1.0
      (stream-map (lambda (guess) (sqrt-improve guess x)) guesses)))
   guesses)
+
+(define (sqrt x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
+
+(sqrt 2 0.000000000000000000000000000001)
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(stream-ref (sqrt-stream 2) 0)
+(stream-ref (sqrt-stream 2) 1)
+(stream-ref (sqrt-stream 2) 2)
+(stream-ref (sqrt-stream 2) 3)
+(stream-ref (sqrt-stream 2) 4)
