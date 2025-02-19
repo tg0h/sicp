0006b627b71d934b96e9fcd798ae916624c87ade
tim goh <timslgoh@gmail.com>
2025-02-19 12:13:58 +0800

    03_modularity_objects_and_state/3.70.rkt

--- 03_modularity_objects_and_state/3.70.rkt
+++ 03_modularity_objects_and_state/3.70.rkt
@@ -96,3 +96,24 @@
                         s1car
                         (merge (stream-cdr s1)
                                (stream-cdr s2)))))))))
+
+(define (merge-weighted weight s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< (weight s1car) (weight s2car)
+                     (cons-stream
+                      s1car
+                      (merge-weighted weight (stream-cdr s1) s2))))
+                 ((> (weight s1car) (weight s2car))
+                  (cons-stream
+                   s2car
+                   (merge-weighted weight s1 (stream-cdr s2))))
+                 (else (cons-stream
+                        s1car
+                        (merge-weighted weight
+                                        (stream-cdr s1)
+                                        (stream-cdr s2)))
+                       ))))))
