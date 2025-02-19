d62425e5bad5ce8d2d8a614e0f5060e4e35ff75e
tim goh <timslgoh@gmail.com>
2025-02-19 12:10:14 +0800

    03_modularity_objects_and_state/3.70.rkt

--- 03_modularity_objects_and_state/3.70.rkt
+++ 03_modularity_objects_and_state/3.70.rkt
@@ -103,3 +103,20 @@
 ;; (stream-for-each pythagorean-triples 1 10)
 
 ;; (stream-for-each (pairs integers (stream-cdr integers)) 1 20)
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1) 
+        (else
+                                (let ((s1car (stream-car s1)) (s2car (stream-car s2)))
+                                  (cond ((< s1car s2car) (cons-stream
+                                                          s1car
+                                                          (merge (stream-cdr s1) s2)))
+                                        ((> s1car s2car)
+                                         (cons-stream
+                                          s2car
+                                          (merge s1 (stream-cdr s2))))
+                                        (else (cons-stream
+                                               s1car
+                                               (merge (stream-cdr s1)
+                                                      (stream-cdr s2)))))))))
