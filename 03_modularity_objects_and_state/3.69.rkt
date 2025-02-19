557868076ced5cff6469ef85b2692102631d0079
tim goh <timslgoh@gmail.com>
2025-02-19 11:15:56 +0800

    03_modularity_objects_and_state/3.69.rkt

---
+++ 03_modularity_objects_and_state/3.69.rkt
@@ -0,0 +1,58 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define
+  (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x)) (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+(define integers (integers-starting-from 1))
+
+(define pii (pairs integers integers))
+
+(define (stream-for-each s n limit)
+  (if (or (stream-null? s )
+          (= n limit )
+          ;; (= (stream-car (stream-car s)) 99)
+          )
+      'done
+      (begin  (display n)
+              (display ": ")
+              (display (stream-car s))
+              (newline)
+              (stream-for-each (stream-cdr s) (+ n 1 ) limit))))
+;; (define (display-line x) (display x)(newline))
+
+;; (define (display-stream s) (stream-for-each display-line s 0))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+;; (stream-for-each pii 1 40)
+
+(define
+
