45ec78b057aef3568ec5fb7fc515b68669bf3b4d
tim goh <timslgoh@gmail.com>
2025-02-20 11:39:04 +0800

    03_modularity_objects_and_state/3.75.rkt

---
+++ 03_modularity_objects_and_state/3.75.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (make-zero-crossings input-stream last-value)
+  (let ((avpt (/ (+ (stream-car input-stream)
+                    last-value)
+                 2)))
+    (cons-stream
+     (sign-change-detector avpt last-value)
+     (make-zero-crossings
+      (stream-cdr input-stream) avpt))))
