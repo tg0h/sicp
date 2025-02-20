a6370909b0d456c0b83cbe6cdbb8c73cb11b9e7a
tim goh <timslgoh@gmail.com>
2025-02-20 12:28:09 +0800

    03_modularity_objects_and_state/section-3.5.4-streams-and-delayed-evaluation.rkt

---
+++ 03_modularity_objects_and_state/section-3.5.4-streams-and-delayed-evaluation.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(define (stream-car stream) (car stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor))
+              stream))
+
+(define
+  (stream-map proc . argstreams)
+  (if (null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+
+(define (integral delayed-integrand initial-value dt) 
+  (define int
+(cons-stream
+initial-value
+(let ((integrand (force delayed-integrand)))
+       (add-streams (scale-stream integrand dt) int))))
+  int)
