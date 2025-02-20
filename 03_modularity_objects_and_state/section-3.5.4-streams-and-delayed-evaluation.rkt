386fc755a40d4e0fcefce851e7d99b6c43e0a71f
tim goh <timslgoh@gmail.com>
2025-02-20 12:28:30 +0800

    03_modularity_objects_and_state/section-3.5.4-streams-and-delayed-evaluation.rkt

--- 03_modularity_objects_and_state/section-3.5.4-streams-and-delayed-evaluation.rkt
+++ 03_modularity_objects_and_state/section-3.5.4-streams-and-delayed-evaluation.rkt
@@ -19,10 +19,15 @@
 (define (add-streams s1 s2) (stream-map + s1 s2))
 
 
-(define (integral delayed-integrand initial-value dt) 
+(define (integral delayed-integrand initial-value dt)
   (define int
-(cons-stream
-initial-value
-(let ((integrand (force delayed-integrand)))
+    (cons-stream
+     initial-value
+     (let ((integrand (force delayed-integrand)))
        (add-streams (scale-stream integrand dt) int))))
   int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt)) 
+  (define dy (stream-map f y))
+  y)
