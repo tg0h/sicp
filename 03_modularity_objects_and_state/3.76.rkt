4eaba531f8501514528f6adfdfd3b8c9b2f760a8
tim goh <timslgoh@gmail.com>
2025-02-20 12:07:01 +0800

    03_modularity_objects_and_state/3.76.rkt

--- 03_modularity_objects_and_state/3.76.rkt
+++ 03_modularity_objects_and_state/3.76.rkt
@@ -3,17 +3,25 @@
 (define (stream-car stream) (car stream))
 (define (stream-cdr stream) (force (cdr stream)))
 
-(define (make-zero-crossings input-stream last-smooth-value last-sense-value)
-  (let ((avpt (/ (+ (stream-car input-stream)
-                    last-sense-value)
-                 2)))
-    (cons-stream
-     (sign-change-detector avpt last-smooth-value)
-     (make-zero-crossings
-      (stream-cdr input-stream) avpt (stream-car input-stream))))
+;; (define (make-zero-crossings input-stream last-smooth-value last-sense-value)
+;;   (let ((avpt (/ (+ (stream-car input-stream)
+;;                     last-sense-value)
+;;                  2)))
+;;     (cons-stream
+;;      (sign-change-detector avpt last-smooth-value)
+;;      (make-zero-crossings
+;;       (stream-cdr input-stream) avpt (stream-car input-stream)))))
 
 (define (smooth s)
-  (let ((current stream-car s)
+  (let ((current (stream-car s))
         (next (stream-car (stream-cdr s))))
     (cons-stream (/ (+ current next) 2)
                  (smooth (stream-cdr s)))))
+
+(define (make-zero-crossings input-stream previous-value)
+  (let ((smooth-value ( stream-car (smooth input-stream))))
+    (cons-stream
+     (sign-change-detector smooth-value last-smooth-value)
+     (make-zero-crossings
+      (stream-cdr input-stream) smooth-value )))
+  )
