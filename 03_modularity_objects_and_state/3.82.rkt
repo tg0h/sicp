177d21acff5e62c543b84fcf765e6160c30437b3
tim goh <timslgoh@gmail.com>
2025-02-21 08:38:08 +0800

    03_modularity_objects_and_state/3.82.rkt

---
+++ 03_modularity_objects_and_state/3.82.rkt
@@ -0,0 +1,59 @@
+#lang sicp
+
+(define (rand)
+  (random 10000)
+  )
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1)
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1)
+                 trials-passed))))
+  (iter trials 0))
+
+;; (monte-carlo 10000 cesaro-test)
+
+
+;; (random-in-range 5.0 10.0)
+
+
+(define (pred x1 x2 y1 y2)
+  (define (square x) (* x x))
+  (define (random-in-range low high)
+    (define range (- high low))
+    (+ low (random range))
+    )
+  (lambda ()
+    (let (
+          (rand_x (random-in-range x1 x2))
+          (rand_y (random-in-range y1 y2))
+          )
+      ;; (display rand_x)
+      ;; (newline)
+      ;; (display rand_y)
+      ;; (newline)
+      (< (+ (square (- rand_x 1))
+            (square (- rand_y 1)))
+         1)))
+  )
+
+(define (estimate-integral pred x1 x2 y1 y2 trials)
+  (* (monte-carlo trials (pred x1 x2 y1 y2)) 4.0)
+  )
+
+(estimate-integral pred 0.0 2.0 0.0 2.0 10000)
+
+;; (pred)
+
+
