5cf37b1266ea92148348f80dd4f802cb20eb4baa
tim goh <timslgoh@gmail.com>
2025-02-24 12:47:40 +0800

    04_metalinguistic_abstraction/4.5.rkt

--- 04_metalinguistic_abstraction/4.5.rkt
+++ 04_metalinguistic_abstraction/4.5.rkt
@@ -5,6 +5,19 @@
       (eq? (car exp) tag)
       false))
 
+; begin
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+(define (make-begin seq) (cons 'begin seq))
+
 ; cond (convert cond to nested ifs)
 (define (cond? exp) (tagged-list? exp 'cond))
 (define (cond-clauses exp) (cdr exp))
@@ -13,7 +26,7 @@
 (define (cond-predicate clause) (car clause))
 (define (cond-actions clause) (cdr clause))
 
-(define (cond->if exp) 
+(define (cond->if exp)
   (expand-clauses (cond-clauses exp)))
 
 (define (expand-clauses clauses)
