36f85a45a24805610b57556495d783010d6249ba
tim goh <timslgoh@gmail.com>
2025-02-24 14:38:21 +0800

    04_metalinguistic_abstraction/4.6.rkt

--- 04_metalinguistic_abstraction/4.6.rkt
+++ 04_metalinguistic_abstraction/4.6.rkt
@@ -1,1 +1,40 @@
 #lang sicp
+
+ 
+(define (eval exp env)
+  (cond ((self-evaluating? exp) ;; primitive - string or number
+         exp)
+        ((variable? exp) ;; is exp a symbol? (not a list starting with the symbol quote)
+         (lookup-variable-value exp env))
+
+        ((quoted? exp) ;; togged-list - quoted - a list starting with the symbol quote
+         (text-of-quotation exp))
+
+        ((assignment? exp) ;; tagged-list - assignment - set! x 2
+         (eval-assignment exp env))
+
+        ((definition? exp) ;; tagged-list - definition - add the variable to the environment, the value can be simple or a lambda
+         (eval-definition exp env))
+
+        ((if? exp) ;; tagged-list - if
+         (eval-if exp env))
+
+        ((lambda? exp) ;; tagged-list - lambda
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+
+        ((begin? exp) ;; tagged-list - begin
+         (eval-sequence
+          (begin-actions exp) env))
+
+        ((cond? exp) ;; tagged-list - cond
+         (eval
+          (cond->if exp) env))
+
+        ((application? exp) ;; function call
+         (meta-apply
+          (eval (operator exp) env)
+          (list-of-values (operands exp) env)))
+        (else
+         (error "Unknown expression type: EVAL" exp))))
