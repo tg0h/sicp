062fdbdd69bb23433ae894f7f3fa1556f0e3e78d
tim goh <timslgoh@gmail.com>
2025-02-24 12:53:51 +0800

    04_metalinguistic_abstraction/4.5.rkt

--- 04_metalinguistic_abstraction/4.5.rkt
+++ 04_metalinguistic_abstraction/4.5.rkt
@@ -33,11 +33,13 @@
 (define (expand-clauses clauses)
   (if (null? clauses)
       'false ; no else clause
-      (let ((first (car clauses)) (rest (cdr clauses)))
-        (if (cond-else-clause? first) (if (null? rest)
-                                          (sequence->exp (cond-actions first))
-                                          (error "ELSE clause isn't last: COND->IF"
-                                                 clauses))
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last: COND->IF"
+                       clauses))
             (make-if (cond-predicate first)
                      (sequence->exp (cond-actions first))
                      (expand-clauses rest))))))
@@ -48,3 +50,6 @@
 
 (define (cond->if exp)
   (expand-clauses (cond-clauses exp)))
+
+(cond ((assoc 'b '((a 1) (b 2))) => cadr)
+      (else false))
