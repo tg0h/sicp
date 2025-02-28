9ef41b071a954b5d026bab0090e555790d99eed2
tim goh <timslgoh@gmail.com>
2025-02-28 10:04:57 +0800

    04_metalinguistic_abstraction/4.13.rkt

--- 04_metalinguistic_abstraction/4.13.rkt
+++ 04_metalinguistic_abstraction/4.13.rkt
@@ -350,29 +350,30 @@
 (define output-prompt ";;; M-Eval value:")
 
 ; input driver loop
-;; (define (driver-loop)
-;;   (prompt-for-input input-prompt)
-;;   ; read returns a list of the complete expression that the user types
-;;   ; eg user types (+ 23 x) -> read returns a list with a SYMBOL, number and SYMBOL (+ 23 x)
-;;   ; eg user types 'x -> read returns list with quote and symbol x (quote x)
-;;   (let ((input (read)))
-;;     (let ((output (eval input the-global-environment)))
-;;       (announce-output output-prompt)
-;;       (user-print output)))
-;;   (driver-loop))
-
-; one-shot driver loop
-(define input-text '(+ 1 3))
-
-(define (one-shot)
-  (let ((input input-text))
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  ; read returns a list of the complete expression that the user types
+  ; eg user types (+ 23 x) -> read returns a list with a SYMBOL, number and SYMBOL (+ 23 x)
+  ; eg user types 'x -> read returns list with quote and symbol x (quote x)
+  (let ((input (read)))
     (let ((output (eval input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
-  ;; (driver-loop)
-  )
+  (driver-loop))
+
+; one-shot driver loop
+;; (define input-text '(+ 1 3))
+
+;; (define (one-shot)
+;;   (let ((input input-text))
+;;     (let ((output (eval input the-global-environment)))
+;;       (announce-output output-prompt)
+;;       (user-print output)))
+;;   ;; (driver-loop)
+;;   )
 
 ;; setup environment sets up the primitives like
 ;; car, cons, +, 'true
 (define the-global-environment (setup-environment))
-(one-shot)
+;; (one-shot)
+(driver-loop)
