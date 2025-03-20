bfa6eb5176cdc6760e1a910ae21ca2a677c81471
tim goh <timslgoh@gmail.com>
2025-03-20 11:08:56 +0800

    04_metalinguistic_abstraction/4.21.rkt

--- 04_metalinguistic_abstraction/4.21.rkt
+++ 04_metalinguistic_abstraction/4.21.rkt
@@ -2,24 +2,24 @@
 
 ; a
 
-((lambda (n)
-   (
-    (lambda (fact) (fact fact n))
-    (lambda (ft k)
-      (if (= k 1)
-          1
-          (* k (ft ft (- k 1))))
-      ))
-   ) 4)
+;; ((lambda (n)
+;;    (
+;;     (lambda (fact) (fact fact n))
+;;     (lambda (ft k)
+;;       (if (= k 1)
+;;           1
+;;           (* k (ft ft (- k 1))))
+;;       ))
+;;    ) 4)
 
-((lambda (n)
-   ((lambda (fib) (fib fib n))
-    (lambda (fb n)
-      (cond
-        ((= n 0) 0)
-        ((= n 1) 1)
-        (else (+ (fb fb (- n 2)) (fb fb (- n 1))))
-        )))) 10)
+;; ((lambda (n)
+;;    ((lambda (fib) (fib fib n))
+;;     (lambda (fb n)
+;;       (cond
+;;         ((= n 0) 0)
+;;         ((= n 1) 1)
+;;         (else (+ (fb fb (- n 2)) (fb fb (- n 1))))
+;;         )))) 10)
 
 (define (fib n)
   (cond
@@ -30,7 +30,7 @@
            (fib (- n 1) )
            ))))
 
-(fib 10)
+;; (fib 10)
 
 ; b
 
@@ -48,4 +48,23 @@
    (lambda (ev? od? n)
      (if (= n 0) false (ev? ev? od? (- n 1))))))
 
-(_f 10)
+;; (_f 10)
+
+; y combinator example
+
+;non-recursive factorial function
+(define fact-once
+  (lambda (f)
+    (lambda (n)
+      (if (= n 0)
+          1
+          (* n (f (- n 1)))))))
+
+;y-combinator
+(define Y
+  (lambda (f)
+    ((lambda (x) (x x))
+     (lambda (x) (f (lambda (y) ((x x) y)))))))
+
+(define factorial (Y fact-once))
+(factorial 20)  ;=2432902008176640000
